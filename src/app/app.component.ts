8d5d06a8d660d439627c53e4cf5fb50d64b0f18c
Jonatan Vicente <it@jonatanvicente.com>
2023-04-26 12:54:40 +0200

    Testing Sonarqube

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ita-challenges-frontend';
+  title = 'ita-challenges';
 }
+
