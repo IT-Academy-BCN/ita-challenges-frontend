794a54a7157a1cbc759fbdb601369632af8e9f81
Jonatan Vicente <it@jonatanvicente.com>
2023-05-02 11:29:49 +0200

    Added starter module

--- src/app/modules/starter/starter.module.ts
+++ src/app/modules/starter/starter.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MainComponent} from "../../core/layout/main/main.component";
 import {StarterRoutingModule} from "./starter-routing.module";
+import {CoreModule} from "../../core/core.module";
 
 @NgModule({
   declarations: [
-    MainComponent
   ],
   imports: [
     CommonModule,
-    StarterRoutingModule
+    StarterRoutingModule,
+    CoreModule
   ]
 })
 export class StarterModule { }
