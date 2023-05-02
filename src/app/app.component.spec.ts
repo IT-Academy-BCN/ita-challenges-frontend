226ea5d9f44c43ac62b4ce2936f3eba5a56d2ee4
Jonatan Vicente <it@jonatanvicente.com>
2023-05-02 12:58:38 +0200

    Fixing tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ita-challenges-frontend'`, () => {
+  it(`should have as title 'ita-challenges'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ita-challenges-frontend');
+    expect(app.title).toEqual('ita-challenges');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ita-challenges-frontend app is running!');
-  });
 });
