11705767fe11180412db0d4ac29ea4241ad15698
Jonatan Vicente <it@jonatanvicente.com>
2023-05-02 13:05:08 +0200

    Including CustomDateAdapter to using Angular Material: Monday first day of week

--- src/app/helpers/CustomDateAdapter.ts
+++ src/app/helpers/CustomDateAdapter.ts
@@ -1,19 +1,19 @@
 import {NativeDateAdapter} from "@angular/material/core";
 import {Injectable} from "@angular/core";
 
-/*@Injectable()
+@Injectable()
 export class CustomDateAdapter extends NativeDateAdapter {
 
-  /!*
+  /*
   0-Sunday
   1-Monday
   2-Tuesday
   3- ...
-   *!/
+   */
 
   getFirstDayOfWeek(): number {
     return 1;
   }
   //TODO - bug en config
 
-}*/
+}
