fd212df0b68da825e4fe8656fbaca50e3b483d54
HelenaPolo <helena.polo@hotmail.es>
2023-05-03 09:42:45 +0200

    F#27- Configuración bootsrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import {DateAdapter, MAT_DATE_LOCALE, MatNativeDateModule} from "@angular/material/core";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {CoreModule} from "./core/core.module";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 //import {CustomDateAdapter} from "./helpers/CustomDateAdapter";
 
 @NgModule({
@@ -16,7 +17,8 @@ import {CoreModule} from "./core/core.module";
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    CoreModule
+    CoreModule,
+    NgbModule
   ],
   providers: [
     { provide: MAT_DATE_LOCALE, useValue: 'es-ES' },
