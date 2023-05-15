db0666746771aabdeb7b73977a92f19a903ea2fe
Jonatan Vicente <it@jonatanvicente.com>
2023-05-15 12:54:21 +0200

    Init observable test

--- src/app/services/starter.service-dummy.spec.ts
+++ src/app/services/starter.service-dummy.spec.ts
@@ -9,44 +9,48 @@ import {TestBed} from "@angular/core/testing";
 
 describe('StarterService', () => {
 
-    let starterService: StarterService;
+    let service: StarterService;
     let httpMock: HttpTestingController;
     let scheduler: TestScheduler;
     let httpClientSpy: any;
+    let testScheduler: TestScheduler;
 
     beforeEach(() => scheduler = new TestScheduler((actual, expected) => {
-        expect(actual).toEqual(expected);
-
-    });
         httpClientSpy = jasmine.createSpy('httpClient');
         httpClientSpy.get = jasmine.createSpy('get').and.returnValue(of('pizza', 'burger', 'hotdog'));
-        //inject spy
-        starterService = new StarterService(httpClientSpy);
+        service = new StarterService(httpClientSpy);
+        expect(actual).toEqual(expected);
+
+    }));
+
+    beforeEach(() => {
+        httpClientSpy = jasmine.createSpy('httpClient');
+        httpClientSpy.get = jasmine.createSpy('get').and.returnValue(of('pizza', 'burger', 'hotdog'));
+        service = new StarterService(httpClientSpy);
+        testScheduler = new TestScheduler((actual, expected) => {
+            expect(actual).toEqual(expected);
+
+        });
+    });
 
 
-
-    );
-
-    it('Should be created', () => {
-        expect(starterService).toBeTruthy()
-    })
-
-
-/*    it('Should stream all challenges', () => {
+    it('Should stream all challenges', () => {
 
         scheduler.run(({expectObservable}) => {
 
             const expectedMarble = '(abc|)';
             const expectedValues = {a: 'pizza', b: 'burger', c: 'hotdog'};
 
-            //pte inyectar observable
-             //const food$ = of('pizza', 'burger', 'hotdog');
-            const food$ = service.getAllChallenges()
+            // const food$ = of('pizza', 'burger', 'hotdog');
+            const food$ = service.getAllChallenges();
+
+
+
 
             expectObservable(food$).toBe(expectedMarble, expectedValues);
         });
 
-    });*/
+    });
 
 
 });
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,8 @@ export const environment = {
   production: false,
   //appVersion: require('../../package.json').version,
   BACKEND_BASE_URL: 'http://localhost:3000/ita-challenges/api',
-  BACKEND_ALL_CHALLENGES: '/challenges'
+  BACKEND_ALL_CHALLENGES: '/challenges',
+  AUTHORIZATION: 'Authorization',
+  BEARER: 'Bearer ',
+  BACKEND_TOKEN: ''
 };
