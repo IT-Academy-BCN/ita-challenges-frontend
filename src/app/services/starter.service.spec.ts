7cc6ad0522b5c5ef2f71d98cb857cc5114480944
Jonatan Vicente <it@jonatanvicente.com>
2023-05-16 12:05:06 +0200

    Adding Observable test

--- src/app/services/starter.service.spec.ts
+++ src/app/services/starter.service.spec.ts
@@ -1,39 +1,70 @@
 import {StarterService} from "./starter.service";
 import { hot, cold } from 'jasmine-marbles'
-import {Observable, of} from "rxjs";
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {TestScheduler} from "rxjs/internal/testing/TestScheduler";
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
+import {TestBed} from "@angular/core/testing";
+import {of} from "rxjs";
+import data from "./../../assets/dummy/data-challenge.json";
 
+
+/* Observable Test */
 describe('StarterService', () => {
 
-    let starterService: StarterService;
+    let service: StarterService;
+    let httpMock: HttpTestingController;
+    let scheduler: TestScheduler;
     let httpClientSpy: any;
+    let testScheduler: TestScheduler;
 
     beforeEach(() => {
-        httpClientSpy = jasmine.createSpy('httpClient');
 
-        //OJO retorna string, NO un observable
-        httpClientSpy.get = jasmine.createSpy('get').and.returnValue({
-            a: 'Spiderman',
-            b: 'Thor',
-            c: 'Lobezno'
+        //inject spy - TODO load data from json file
+        httpClientSpy = jasmine.createSpy('httpClient');
+        httpClientSpy.get = jasmine.createSpy('get').and.returnValue(of('pizza', 'burger', 'hotdog'));
+        service = new StarterService(httpClientSpy);
+        testScheduler = new TestScheduler((actual, expected) => {
+            expect(actual).toEqual(expected);
+
         });
-        //inject spy
-        starterService = new StarterService(httpClientSpy);
     });
 
-    it('Should be created', () => {
-        expect(starterService).toBeTruthy()
-    })
 
-    it('Should correctly return all challenges', () => {
-        const resultExpected = {
-            a: 'Spiderman',
-            b: 'Thor',
-            c: 'Lobezno',
-        }
-        //comparacion de strings
-        expect(starterService.getAllChallenges()).toStrictEqual(resultExpected);
-    })
+    /*
+    Some explanations:
+    RxJs introduced the following syntax when writing marble tests in our code
+        - ' ' the whitespace is a unique character that will not be interpreted; it can be used to align your marble string.
+        - '-' represents a frame of virtual time passing
+        - '|' This sign illustrates the completion of an observable.
+        - '#' Signifies an error
+        - [a-z] an alphanumeric character represents a value which is emitted by the Observable.
+        - '()' used to group events in the same frame. This can be used to group values, errors, and completion.
+        - '^' this sign illustrates the subscription point and will only be used when we are dealing with hot observables.
+
+    That’s the basic syntax. Let’s look at some examples to make ourself more familiar with the syntax.
+        - --: equivalent to NEVER. An observable that never emits
+        - a--b--c| : an Observable that emits a on the first frame, b on the fourth and c on the seventh. After emitting c the observable completes.
+        - ab--# : An Observable that emits a on frame two, b on frame three and an error on frame six.
+        - a^(bc)--|: A hot Observable that emits a before the subscription.
+     */
+
+    it('Should stream all challenges', () => {
+
+        testScheduler.run(({expectObservable}) => {
+
+            const expectedMarble = '(abc|)';
+            const expectedValues = {a: 'pizza', b: 'burger', c: 'hotdog'};
+            const obs$ = service.getAllChallenges();
+            console.log(data);
+            expectObservable(obs$).toBe(expectedMarble, expectedValues);
+        });
+
+    });
+
 });
+
+
+
+
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  //appVersion: require('../../package.json').version,
+  //appVersion: require('../../package.dummy').version,
   BACKEND_BASE_URL: 'http://localhost:3000/ita-challenges/api',
   BACKEND_ALL_CHALLENGES: '/challenges',
   AUTHORIZATION: 'Authorization',
