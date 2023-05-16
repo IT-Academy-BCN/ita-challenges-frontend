b16d4551a809c2e1ac400da6acedae035523cdf5
HelenaPolo <helena.polo@hotmail.es>
2023-05-16 10:29:10 +0200

    estructura challenge-module

---
+++ src/app/modules/challenge/challenge-header/challenge-header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-challenge-header',
+  templateUrl: './challenge-header.component.html',
+  styleUrls: ['./challenge-header.component.scss']
+})
+export class ChallengeHeaderComponent {
+
+}
--- src/app/modules/challenge/challenge-routing.module.ts
+++ src/app/modules/challenge/challenge-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {ChallengeComponent} from "../../shared/components/challenge/challenge.component";
+import { StarterComponent } from '../starter/starter/starter.component';
 
 const routes: Routes = [];
 
