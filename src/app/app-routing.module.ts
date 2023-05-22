2a3e3b2f3743339049b2c45907eb832ae5694847
Helena Polo <helena.polo@hotmail.es>
2023-05-22 13:49:06 +0200

    cambio ruta inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/ita-challenge'}
+  { path: '', pathMatch: 'full', redirectTo: '/ita-challenge/challenge-list'}
 ];
 
 @NgModule({
