fc39464af15dd5768fab85a3324f0a737393e8fa
Helena Polo <helena.polo@hotmail.es>
2023-05-23 11:13:40 +0200

    adecuación pestaña relacionado

--- src/app/shared/components/shared-components.module.ts
+++ src/app/shared/components/shared-components.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import {ChallengeComponent} from "../../shared/components/challenge/challenge.component";
 
+
 @NgModule({
     declarations: [
-        ChallengeComponent
+        ChallengeComponent,
     ],
     imports: [],
     exports: [
