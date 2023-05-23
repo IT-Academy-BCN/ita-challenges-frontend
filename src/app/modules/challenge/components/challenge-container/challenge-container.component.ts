bea1128b7a90179647df2e1de9538b0502b7abcb
Alberto Almenara <alberto.almenara@gmail.com>
2023-05-23 16:36:27 +0200

    Captura de id en challenge-container

--- src/app/modules/challenge/components/challenge-container/challenge-container.component.ts
+++ src/app/modules/challenge/components/challenge-container/challenge-container.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-challenge-container',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./challenge-container.component.scss']
 })
 export class ChallengeContainerComponent {
+  idChallenge!: string | null;
+  params$!: Subscription
 
+  constructor(
+    private route: ActivatedRoute
+  ){
+    this.params$ =  this.route.paramMap.subscribe((params: ParamMap) => {
+      this.idChallenge = params.get('idChallenge')
+      console.log(this.idChallenge)
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.params$ != undefined) this.params$.unsubscribe();
+  }
 }
