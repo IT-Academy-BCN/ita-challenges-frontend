f528cc065115518ecc93c45ac609a6ef66a833b8
Alberto Almenara <alberto.almenara@gmail.com>
2023-05-24 12:30:45 +0200

    corrección 'code smell'

--- src/app/modules/challenge/components/challenge-container/challenge-container.component.ts
+++ src/app/modules/challenge/components/challenge-container/challenge-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Subscription } from 'rxjs';
 
