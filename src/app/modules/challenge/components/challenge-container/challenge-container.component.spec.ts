8e5a83f0a1b0ba12fffcded566d519f83e96198c
Alberto Almenara <alberto.almenara@gmail.com>
2023-05-25 12:35:53 +0200

    corrección testing

--- src/app/modules/challenge/components/challenge-container/challenge-container.component.spec.ts
+++ src/app/modules/challenge/components/challenge-container/challenge-container.component.spec.ts
@@ -3,7 +3,6 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ChallengeContainerComponent } from './challenge-container.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { ActivatedRoute } from '@angular/router';
 
 describe('ChallengeContainerComponent', () => {
   let component: ChallengeContainerComponent;
