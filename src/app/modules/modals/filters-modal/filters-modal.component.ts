f731ade811ac3228c8d28667529e49f05bbf18c8
Roger Miret <r.mir.min@gmail.com>
2023-07-04 12:29:06 +0200

    feat(modals-module): create filters-modal and add to declarations/exports

---
+++ src/app/modules/modals/filters-modal/filters-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filters-modal',
+  templateUrl: './filters-modal.component.html',
+  styleUrls: ['./filters-modal.component.scss']
+})
+export class FiltersModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
