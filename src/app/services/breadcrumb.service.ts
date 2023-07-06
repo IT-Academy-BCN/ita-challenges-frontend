ec500616c03d0ab329b50f3a199203b9f21e4ffb
Roger Miret <r.mir.min@gmail.com>
2023-07-06 18:28:04 +0200

    feat(services): create breadcrumb service

---
+++ src/app/services/breadcrumb.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, filter } from 'rxjs';
+import { Breacrumb } from '../models/interfaces';
+import { Router, NavigationEnd, ActivatedRouteSnapshot, Data } from '@angular/router';
+
+@Injectable({ 
+  providedIn: 'root' 
+}) 
+export class BreadcrumbService { 
+ 
+  // Subject emitting the breadcrumb hierarchy 
+  private readonly _breadcrumbs$ = new BehaviorSubject<Breacrumb[]>([]); 
+ 
+  // Observable exposing the breadcrumb hierarchy 
+  readonly breadcrumbs$ = this._breadcrumbs$.asObservable(); 
+ 
+  constructor(private router: Router) { 
+    this.router.events.pipe( 
+      // Filter the NavigationEnd events as the breadcrumb is updated only when the route reaches its end 
+      filter((event) => event instanceof NavigationEnd) 
+    ).subscribe(event => { 
+      // Construct the breadcrumb hierarchy 
+      const root = this.router.routerState.snapshot.root; 
+      const breadcrumbs: Breacrumb[] = []; 
+      this.addBreadcrumb(root, [], breadcrumbs); 
+ 
+      // Emit the new hierarchy 
+      this._breadcrumbs$.next(breadcrumbs); 
+    }); 
+  } 
+ 
+  private addBreadcrumb(route: ActivatedRouteSnapshot, parentUrl: string[], breadcrumbs: Breacrumb[]) { 
+    if (route) { 
+      // Construct the route URL 
+      const routeUrl = parentUrl.concat(route.url.map(url => url.path)); 
+ 
+      // Add an element for the current route part 
+      if (route.data['breadcrumb']) { 
+        const breadcrumb = { 
+          label: this.getLabel(route.data), 
+          url: '/' + routeUrl.join('/') 
+        }; 
+        breadcrumbs.push(breadcrumb); 
+      } 
+ 
+      // Add another element for the next route part 
+      this.addBreadcrumb(route.firstChild, routeUrl, breadcrumbs); 
+    } 
+  } 
+ 
+  private getLabel(data: Data) { 
+    // The breadcrumb can be defined as a static string or as a function to construct the breadcrumb element out of the route data 
+    return typeof data['breadcrumb'] === 'function' ? data['breadcrumb'](data) : data['breadcrumb']; 
+  } 
+ 
+} 
