eedb1f3620d66cb51e79d15700478498971e9053
Roger Miret <r.mir.min@gmail.com>
2023-07-07 13:19:49 +0200

    feat(challenge-info component): coordinate selected tab with url (as query params). Use ngBootstrap directives in the template and also add three functions to .ts.

--- src/app/modules/challenge/challenge.module.ts
+++ src/app/modules/challenge/challenge.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {SharedComponentsModule} from "../../shared/components/shared-components.module";
+import { SharedComponentsModule } from "../../shared/components/shared-components.module";
 import { ChallengeHeaderComponent } from './components/challenge-header/challenge-header.component';
 import { ChallengeInfoComponent } from './components/challenge-info/challenge-info.component';
 import { ChallengeContainerComponent } from './components/challenge-container/challenge-container.component';
 import { ChallengeRoutingModule } from './challenge-routing.module';
 import { CoreModule } from 'src/app/core/core.module';
 import { RouterModule } from '@angular/router';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
@@ -22,6 +23,7 @@ import { RouterModule } from '@angular/router';
     CoreModule,
     ChallengeRoutingModule,
     RouterModule,
+    NgbNavModule
   ]
 })
 export class ChallengeModule { }
--- src/app/modules/challenge/components/challenge-info/challenge-info.component.ts
+++ src/app/modules/challenge/components/challenge-info/challenge-info.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { NgbNavChangeEvent, NgbNav } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-challenge-info',
@@ -6,5 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./challenge-info.component.scss']
 })
 export class ChallengeInfoComponent {
+  @ViewChild('nav') nav!: NgbNav;
+
+  initialActiveId = 1;
+
+  tabNames: any = {
+  1: 'details',
+  2: 'solutions',
+  3: 'resources',
+  4: 'related'
+};
+
+  constructor(private router: Router, private route: ActivatedRoute) { }
+
+    ngAfterViewInit() {
+    this.route.queryParams.subscribe(params => {
+      const tab = params['tab'];
+      if (tab) {
+        const tabId = this.getTabId(tab);
+        this.nav.select(tabId);
+      }
+    });
+  }
+
+  onNavChange(changeEvent: NgbNavChangeEvent) {
+    this.router.navigate([], { queryParams: { tab: this.tabNames[changeEvent.nextId] }});
+  }
+
+getTabId(tabName: string): number {
+  const tabId = Object.keys(this.tabNames).find(key => this.tabNames[key] === tabName);
+  return tabId ? Number(tabId) : this.initialActiveId;
+}
+
+
 
 }
