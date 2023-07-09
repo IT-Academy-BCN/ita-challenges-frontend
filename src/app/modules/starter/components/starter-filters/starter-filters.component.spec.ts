0f350a9b94bd51a3ebc5b6fe5b6a8cb84d54c8dc
Roger Miret <r.mir.min@gmail.com>
2023-07-10 00:38:30 +0200

    test(starter.component.spec.ts): add specs to test communication with starter-filters (child component)

--- src/app/modules/starter/components/starter-filters/starter-filters.component.spec.ts
+++ src/app/modules/starter/components/starter-filters/starter-filters.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 import { StarterFiltersComponent } from './starter-filters.component';
 
@@ -29,12 +30,16 @@ describe('StarterFiltersComponent', () => {
   it('should emit filtersSelected event when form value changes', () => {
     spyOn(component.filtersSelected, 'emit');
 
-    component.filtersForm.patchValue({
-      languages: { javascript: true, java: false, php: false, python: false },
-      levels: { easy: true, medium: false, hard: false },
-      progress: { noStarted: true, started: false, finished: false },
-    });
+    const languageInput: HTMLInputElement = fixture.debugElement.query(By.css('#checkJs')).nativeElement;
+    languageInput.click();
+    fixture.detectChanges();
 
+    const levelInput: HTMLInputElement = fixture.debugElement.query(By.css('#checkEasy')).nativeElement;
+    levelInput.click();
+    fixture.detectChanges();
+
+    const progressInput: HTMLInputElement = fixture.debugElement.query(By.css('#checkNoStarted')).nativeElement;
+    progressInput.click();
     fixture.detectChanges();
 
     expect(component.filtersSelected.emit).toHaveBeenCalled();
