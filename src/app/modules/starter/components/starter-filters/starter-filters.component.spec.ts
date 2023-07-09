401aad422756949adbe1c25fc6ef1f606986f2b2
Roger Miret <r.mir.min@gmail.com>
2023-07-09 23:44:25 +0200

    test(starter-filters.spec.ts): adapt testing to reactive forms

--- src/app/modules/starter/components/starter-filters/starter-filters.component.spec.ts
+++ src/app/modules/starter/components/starter-filters/starter-filters.component.spec.ts
@@ -1,28 +1,49 @@
-import { FilterChallenge } from 'src/app/models/filter-challenge.model';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
 
 import { StarterFiltersComponent } from './starter-filters.component';
 
 describe('StarterFiltersComponent', () => {
   let component: StarterFiltersComponent;
   let fixture: ComponentFixture<StarterFiltersComponent>;
-  let filters: FilterChallenge = {languages:[], levels: [], progress: []}
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [StarterFiltersComponent],
+      imports: [ReactiveFormsModule],
+      providers: [FormBuilder]
+    })
+    .compileComponents();
+  });
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [StarterFiltersComponent]
-    });
     fixture = TestBed.createComponent(StarterFiltersComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should return filters array object', () => {
-    expect(component.getAllFilters()).toEqual(filters);
+  it('should emit filtersSelected event when form value changes', () => {
+    spyOn(component.filtersSelected, 'emit');
+
+    component.filtersForm.patchValue({
+      languages: { javascript: true, java: false, php: false, python: false },
+      levels: { easy: true, medium: false, hard: false },
+      progress: { noStarted: true, started: false, finished: false },
+    });
+
+    fixture.detectChanges();
+
+    expect(component.filtersSelected.emit).toHaveBeenCalled();
+
+    const expectedFilter = {
+      languages: [0],
+      levels: ['easy'],
+      progress: [0]
+    };
+    expect(component.filtersSelected.emit).toHaveBeenCalledWith(expectedFilter);
   });
 });
