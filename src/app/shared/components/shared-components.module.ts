03e78b5437a22b82f78b95b32f67bdc0118db5d5
Helena Polo <helena.polo@hotmail.es>
2023-07-12 15:44:09 +0200

    correccion errores test

--- src/app/shared/components/shared-components.module.ts
+++ src/app/shared/components/shared-components.module.ts
@@ -2,26 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 import { CommonModule } from '@angular/common';
-<<<<<<< HEAD
-import { ChallengeComponent } from "../../shared/components/challenge/challenge.component";
-import { ResourceComponent } from './resource/resource.component';
 import { PaginationComponent } from './pagination/pagination.component';
-
-@NgModule({
-    declarations: [
-        ChallengeComponent,
-        ResourceComponent,
-        PaginationComponent
-=======
 import { ChallengeCardComponent } from "./challenge-card/challenge-card.component";
 import { ResourceCardComponent } from './resource-card/resource-card.component';
 
 
 @NgModule({
     declarations: [
+        PaginationComponent,
         ChallengeCardComponent,
         ResourceCardComponent
->>>>>>> c5c397e8b26e47de4a5b19212834e46a70498d1a
     ],
     imports: [
         CommonModule,
@@ -29,14 +19,9 @@ import { ResourceCardComponent } from './resource-card/resource-card.component';
         NgbTooltipModule
     ],
     exports: [
-<<<<<<< HEAD
-        ChallengeComponent,
-        ResourceComponent,
-        PaginationComponent
-=======
+        PaginationComponent,
         ChallengeCardComponent,
         ResourceCardComponent
->>>>>>> c5c397e8b26e47de4a5b19212834e46a70498d1a
     ],  
 })
 export class SharedComponentsModule { }
