3fb4e8bcc46ab6b1e22f6755452810329c939be6
Jonatan Vicente <it@jonatanvicente.com>
2023-07-12 11:07:42 +0200

    Renaming dev version

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  appVersion: require('../../package.json').version + '-dev',
+  appVersion: require('../../package.json').version,
   BACKEND_BASE_URL: 'http://localhost:3000/ita-challenges/api',
   BACKEND_ALL_CHALLENGES: '/challenges',
   AUTHORIZATION: 'Authorization',
