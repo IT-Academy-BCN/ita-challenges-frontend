f1626b421d4381158df35cb40c73a17f706e89e4
Roger Miret <r.mir.min@gmail.com>
2023-07-20 11:10:51 +0200

    test(starter-service): simplify and adapt to changes

--- src/app/services/starter.service.spec.ts
+++ src/app/services/starter.service.spec.ts
@@ -1,65 +1,54 @@
-import {StarterService} from "./starter.service";
-import {TestScheduler} from "rxjs/internal/testing/TestScheduler";
-import {HttpTestingController} from "@angular/common/http/testing";
-import {delay, of} from "rxjs";
-import data from "./../../assets/dummy/data-challenge.json"; //see data-typings.d.ts
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { StarterService } from './starter.service';
+import dataDummy from "./../../assets/dummy/data-challenge.json";
+import { environment } from '../../environments/environment';
 
 
-/* Observable Test, see https://docs.angular.lat/guide/testing-components-scenarios */
 describe('StarterService', () => {
+  let service: StarterService;
+  let httpMock: HttpTestingController;
 
-    let service: StarterService;
-    let httpMock: HttpTestingController;
-    let scheduler: TestScheduler;
-    let httpClientSpy: any;
-    let testScheduler: TestScheduler;
-    
 
-    beforeEach(() => {
-
-        //inject spy
-        httpClientSpy = jasmine.createSpy('httpClient');
-        httpClientSpy.get = jasmine.createSpy('get').and.returnValue(of(data));
-        service = new StarterService(httpClientSpy);
-        testScheduler = new TestScheduler((actual, expected) => {
-            expect(actual).toEqual(expected);
-
-        });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [StarterService]
     });
 
 
-    /*
-    Some explanations:
-    RxJs introduced the following syntax when writing marble tests in our code
-        - ' ' the whitespace is a unique character that will not be interpreted; it can be used to align your marble string.
-        - '-' represents a frame of virtual time passing
-        - '|' This sign illustrates the completion of an observable.
-        - '#' Signifies an error
-        - [a-z] an alphanumeric character represents a value which is emitted by the Observable.
-        - '()' used to group events in the same frame. This can be used to group values, errors, and completion.
-        - '^' this sign illustrates the subscription point and will only be used when we are dealing with hot observables.
+    service = TestBed.inject(StarterService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
 
-    That’s the basic syntax. Let’s look at some examples to make ourself more familiar with the syntax.
-        - --: equivalent to NEVER. An observable that never emits
-        - a--b--c| : an Observable that emits a on the first frame, b on the fourth and c on the seventh. After emitting c the observable completes.
-        - ab--# : An Observable that emits a on frame two, b on frame three and an error on frame six.
-        - a^(bc)--|: A hot Observable that emits a before the subscription.
-     */
 
-    it('Should stream all challenges', () => {
+  afterEach(() => {
+    httpMock.verify(); // Make sure that there are no outstanding requests.
+  });
 
-        testScheduler.run(({expectObservable}) => {
 
-            const expectedMarble = '---(a|)';
-            const expectedValues = {a: data};
-            const obs$ = service.getAllChallenges(1, 10).pipe(delay(3));
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
 
-            expectObservable(obs$).toBe(expectedMarble, expectedValues);
-        });
 
+    it('should get all challenges', () => {
+        const page = 1;
+        const pageSize = environment.pageSize;
+      const dummyData: any = [dataDummy];
+
+
+    service.getAllChallenges(page, pageSize).subscribe(data => {
+      expect(data).toEqual(dummyData);
     });
 
+
+    const req = httpMock.expectOne(`../assets/dummy/data-challenge.json?page=${page}&pageSize=${pageSize}`);
+    expect(req.request.method).toBe('GET');
+    req.flush(dummyData);
+  });
 });
+
 
 
 
