dd23788b7ba250f841e6c6d365b7db130d677f8e
Roger Miret <r.mir.min@gmail.com>
2023-07-25 10:55:52 +0200

    test(send-solution-modal): add specs

--- src/app/modules/modals/send-solution-modal/send-solution-modal.component.spec.ts
+++ src/app/modules/modals/send-solution-modal/send-solution-modal.component.spec.ts
@@ -1,10 +1,13 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, inject } from '@angular/core/testing';
 
 import { SendSolutionModalComponent } from './send-solution-modal.component';
+import { By } from '@angular/platform-browser';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 describe('SendSolutionModalComponent', () => {
   let component: SendSolutionModalComponent;
   let fixture: ComponentFixture<SendSolutionModalComponent>;
+  let modalService: NgbModal;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -13,9 +16,17 @@ describe('SendSolutionModalComponent', () => {
     fixture = TestBed.createComponent(SendSolutionModalComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    modalService = TestBed.inject(NgbModal);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should open modal', () => {
+    spyOn(modalService, 'dismissAll').and.callThrough();
+    fixture.debugElement.query(By.css('[data-testid="closeModal"]')).triggerEventHandler('click', null);
+    fixture.detectChanges();
+    expect(modalService.dismissAll).toHaveBeenCalled();
   });
 });
