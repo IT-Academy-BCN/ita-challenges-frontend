9a3d101e26615315065a8f90b51bdd7de7b4a7fb
Roger Miret <r.mir.min@gmail.com>
2023-07-26 09:51:31 +0200

    breadcrumb-service: change > for parentheses with tab breadcrumb

--- src/app/services/breadcrumb.service.ts
+++ src/app/services/breadcrumb.service.ts
@@ -48,7 +48,7 @@ private addBreadcrumb(route: ActivatedRouteSnapshot, parentUrl: string[], breadc
         } else {
             // Check if route has query param tab (challenge-info selected tab)
             if(route.queryParamMap.has('tab')) {
-                breadcrumbs[breadcrumbs.length - 1].label += ' > ' + route.queryParamMap.get('tab');
+              breadcrumbs[breadcrumbs.length - 1].label += ' (' + route.queryParamMap.get('tab') + ')';
               console.log(route.queryParamMap.get('tab'));
             }
         }
