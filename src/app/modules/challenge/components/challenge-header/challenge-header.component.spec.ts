0b81edcdba991b265e49ad89299218a2b54835bb
Roger Miret <r.mir.min@gmail.com>
2023-07-26 10:09:18 +0200

    add breadcrumb service test, add other tests

--- src/app/modules/challenge/components/challenge-header/challenge-header.component.spec.ts
+++ src/app/modules/challenge/components/challenge-header/challenge-header.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { I18nModule } from '../../../../../assets/i18n/i18n.module';
 import { ChallengeHeaderComponent } from './challenge-header.component';
 import { SendSolutionService } from '../../../../services/send-solution.service';
+import { SolutionComponent } from '../../../../shared/components/solution/solution.component';
 
 describe('ChallengeHeaderComponent', () => {
   let component: ChallengeHeaderComponent;
@@ -9,7 +10,7 @@ describe('ChallengeHeaderComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ChallengeHeaderComponent],
+      declarations: [ChallengeHeaderComponent, SolutionComponent],
       imports: [I18nModule],
       providers: [SendSolutionService]
     });
--- src/app/services/breadcrumb.service.spec.ts
+++ src/app/services/breadcrumb.service.spec.ts
@@ -36,91 +36,90 @@ describe('BreadcrumbService', () => {
 });
 
 
-//     it('should emit breadcrumb when there is a NavigationEnd event (with 1 route segment)', (done) => {
-//         service.breadcrumbs$.subscribe((breadcrumbs) => {
-//             expect(breadcrumbs).toEqual([{
-//                 label: 'breadcrumb label',
-//                 url: '/test'
-//             }]);
-//             done();
-//         });
+    it('should emit breadcrumb when there is a NavigationEnd event (with 1 route segment)', (done) => {
+        service.breadcrumbs$.subscribe((breadcrumbs) => {
+            expect(breadcrumbs).toEqual([{
+                label: 'breadcrumb label',
+                url: '/test'
+            }]);
+            done();
+        });
 
-// const fakeSnapshot: ActivatedRouteSnapshot = {
-//     url: [{ path: 'test' }],
-//     data: {
-//         breadcrumb: 'breadcrumb label'
-//     },
-//     queryParamMap: {
-//         has: () => false, // No query params
-//         get: () => null, // No query params
-//     },
-//     firstChild: null
-// } as any;
+const fakeSnapshot: ActivatedRouteSnapshot = {
+    url: [{ path: 'test' }],
+    data: {
+        breadcrumb: 'breadcrumb label'
+    },
+    queryParamMap: {
+        has: () => false, // No query params
+        get: () => null, // No query params
+    },
+    firstChild: null
+} as any;
         
-//         routerMock.routerState.snapshot.root = fakeSnapshot;
+        routerMock.routerState.snapshot.root = fakeSnapshot;
 
-//         // Simulate NavigationEnd event
-//         eventsSubject.next(new NavigationEnd(1, '/test', '/test'));
-//     });
+        // Simulate NavigationEnd event
+        eventsSubject.next(new NavigationEnd(1, '/test', '/test'));
+    });
 
-//     it('should emit breadcrumb when there is a NavigationEnd event (with multiple route segments)', (done) => {
-//     service.breadcrumbs$.subscribe((breadcrumbs) => {
-//         expect(breadcrumbs).toEqual([
-//             { label: 'breadcrumb label 1', url: '/test1' },
-//             { label: 'breadcrumb label 2', url: '/test1/test2' },
-//             { label: 'breadcrumb label 3', url: '/test1/test2/test3' }
-//         ]);
-//         done();
-//     });
+    it('should emit breadcrumb when there is a NavigationEnd event (with multiple route segments)', (done) => {
+    service.breadcrumbs$.subscribe((breadcrumbs) => {
+        expect(breadcrumbs).toEqual([
+            { label: 'breadcrumb label 1', url: '/test1' },
+            { label: 'breadcrumb label 2', url: '/test1/test2' },
+            { label: 'breadcrumb label 3', url: '/test1/test2/test3' }
+        ]);
+        done();
+    });
 
-//     const fakeSnapshotChild3: ActivatedRouteSnapshot = {
-//         url: [{ path: 'test3' }],
-//         data: {
-//             breadcrumb: 'breadcrumb label 3'
-//         },
-//         queryParamMap: {
-//         has: () => false, // No query params
-//         get: () => null, // No query params
-//         },
-//         firstChild: null
-//         } as any;
+    const fakeSnapshotChild3: ActivatedRouteSnapshot = {
+        url: [{ path: 'test3' }],
+        data: {
+            breadcrumb: 'breadcrumb label 3'
+        },
+        queryParamMap: {
+        has: () => false,
+        get: () => null,
+        },
+        firstChild: null
+        } as any;
         
-//     const fakeSnapshotChild2: ActivatedRouteSnapshot = {
-//         url: [{ path: 'test2' }],
-//         data: {
-//             breadcrumb: 'breadcrumb label 2'
-//         },
-//         queryParamMap: {
-//         has: () => false, // No query params
-//         get: () => null, // No query params
-//         },
-//         firstChild: fakeSnapshotChild3
-//     } as any;
+    const fakeSnapshotChild2: ActivatedRouteSnapshot = {
+        url: [{ path: 'test2' }],
+        data: {
+            breadcrumb: 'breadcrumb label 2'
+        },
+        queryParamMap: {
+        has: () => false,
+        get: () => null,
+        },
+        firstChild: fakeSnapshotChild3
+    } as any;
 
-//     const fakeSnapshot1: ActivatedRouteSnapshot = {
-//         url: [{ path: 'test1' }],
-//         data: {
-//             breadcrumb: 'breadcrumb label 1'
-//         },
-//         queryParamMap: {
-//         has: () => false, // No query params
-//         get: () => null, // No query params
-//         },
-//         firstChild: fakeSnapshotChild2
-//     } as any;
+    const fakeSnapshot1: ActivatedRouteSnapshot = {
+        url: [{ path: 'test1' }],
+        data: {
+            breadcrumb: 'breadcrumb label 1'
+        },
+        queryParamMap: {
+        has: () => false,
+        get: () => null,
+        },
+        firstChild: fakeSnapshotChild2
+    } as any;
 
-//     routerMock.routerState.snapshot.root = fakeSnapshot1;
+    routerMock.routerState.snapshot.root = fakeSnapshot1;
 
-//     // Simulate NavigationEnd event
-//     eventsSubject.next(new NavigationEnd(1, '/test1/test2/test3', '/test1/test2/test3'));
-//     });
+    eventsSubject.next(new NavigationEnd(1, '/test1/test2/test3', '/test1/test2/test3'));
+    });
     
     it('should emit breadcrumb when there is a NavigationEnd event (with multiple route segments + query params)', (done) => {
     service.breadcrumbs$.subscribe((breadcrumbs) => {
         expect(breadcrumbs).toEqual([
             { label: 'breadcrumb label 1', url: '/test1' },
             { label: 'breadcrumb label 2', url: '/test1/test2' },
-            { label: 'breadcrumb label 3 > query', url: '/test1/test2/test3' }
+            { label: 'breadcrumb label 3 (query)', url: '/test1/test2/test3' }
         ]);
         done();
     });
