8f93ebb8849296d9d920d8747afb076af057fa31
valerioprds <v.paradiso89@gmail.com>
2023-10-25 12:07:22 +0200

    changed position of settings and my space and removed settings from top-right

--- src/app/core/layout/main-menu/main-menu.component.ts
+++ src/app/core/layout/main-menu/main-menu.component.ts
@@ -1,27 +1,31 @@
-import { Component } from '@angular/core';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { LoginModalComponent } from './../../../modules/modals/login-modal/login-modal.component';
-import { TranslateService } from '@ngx-translate/core';
+import { Component } from "@angular/core";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { LoginModalComponent } from "./../../../modules/modals/login-modal/login-modal.component";
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
-  selector: 'app-main-menu',
-  templateUrl: './main-menu.component.html',
-  styleUrls: ['./main-menu.component.scss']
+	selector: "app-main-menu",
+	templateUrl: "./main-menu.component.html",
+	styleUrls: ["./main-menu.component.scss"],
 })
 export class MainMenuComponent {
-  
-  constructor(private modalService: NgbModal,
-              private translate: TranslateService){
-                translate.addLangs(['en', 'es', 'cat']);
-                translate.setDefaultLang('es');
-                translate.use('es'); 
-              } 
-  
-  openLoginModal(){
-    this.modalService.open(LoginModalComponent, { centered : true, size : 'lg' })
-  }
+	constructor(
+		private modalService: NgbModal,
+		private translate: TranslateService
+	) {
+		translate.addLangs(["en", "es", "cat"]);
+		translate.setDefaultLang("es");
+		translate.use("es");
+	}
 
-  changeLanguage(language: string): void {
-    this.translate.use(language);
-  }
+	openLoginModal() {
+		this.modalService.open(LoginModalComponent, {
+			centered: true,
+			size: "lg",
+		});
+	}
+
+	changeLanguage(language: string): void {
+		this.translate.use(language);
+	}
 }
