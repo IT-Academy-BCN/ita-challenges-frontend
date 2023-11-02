c8640059072b70bdf5a7e242ea9913ebfdb355eb
Lorena Ponsico <lorena.poca@gmail.com>
2023-11-02 18:40:06 +0100

    Taking login form values to send to authService

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -7,79 +7,87 @@ import { User } from "../models/user.model";
 
 @Injectable()
 export class AuthService {
-	newUser: User | undefined;
+    newUser: User | undefined;
 
-	/**
-	 * Gives us access to the Angular HTTP client so we can make requests to
-	 * our Express app
-	 */
-	constructor(private http: HttpClient) {}
+    /**
+     * Gives us access to the Angular HTTP client so we can make requests to
+     * our Express app
+     */
+    constructor(private http: HttpClient) { }
 
-	/**
-	 * Passes the username and password that the user typed into the application
-	 * and sends a POST request to our Express server login route, which will
-	 * authenticate the credentials and return a JWT token if they are valid
-	 *
-	 * The `res` object (has our JWT in it) is passed to the setLocalStorage
-	 * method below
-	 *
-	 * shareReplay() documentation - https://www.learnrxjs.io/operators/multicasting/sharereplay.html
-	 */
-	login(email: string, password: string) {
-		//return this.http.post();
-	}
+    /**
+     * Passes the username and password that the user typed into the application
+     * and sends a POST request to our Express server login route, which will
+     * authenticate the credentials and return a JWT token if they are valid
+     *
+     * The `res` object (has our JWT in it) is passed to the setLocalStorage
+     * method below
+     *
+     * shareReplay() documentation - https://www.learnrxjs.io/operators/multicasting/sharereplay.html
+     */
+    login(dni: string, password: string): Observable<User> {
+        return this.http.post<User>(`${environment.BACKEND_ITA_WIKI_BASE_URL}${environment.BACKEND_LOGIN}`, { dni, password })
+            .pipe(
+                map((user) => {
+                    console.log(user)
+                    return user;
+                })
 
-	register(user: User): Observable<void> {
-    const userJSON = JSON.stringify(user)
-    console.log('from auth service register', userJSON)
-		return this.http
-			.post<void>(
-				`${environment.BACKEND_ITA_WIKI_BASE_URL}${environment.BACKEND_REGISTER}`,
-				userJSON  // transformar en JSON
-        
-			)
-			.pipe(
-				map((authResult: any) => {
-					this.setLocalStorage(authResult); // Llama a setLocalStorage con el resultado de autenticación
-					console.log('from auth service ', authResult);
-				}),
-				catchError((error: HttpErrorResponse) => {
-					//  Handle error here (show an error message)
-					return throwError(error);
-				})
-			);
-	}
+            );
+    }
 
-	private setLocalStorage(authResult: any) {
-		// Takes the JWT expiresIn value and add that number of seconds
-		// to the current "moment" in time to get an expiry date
-		const expiresAt = moment().add(authResult.expiresIn, "second");
+    register(user: User): Observable<void> {
+        const userJSON = JSON.stringify(user)
+        console.log('from auth service register', userJSON)
+        return this.http
+            .post<void>(
+                `${environment.BACKEND_ITA_WIKI_BASE_URL}${environment.BACKEND_REGISTER}`,
+                userJSON  // transformar en JSON
 
-		// Stores our JWT token and its expiry date in localStorage
-		localStorage.setItem("id_token", authResult.idToken);
-		localStorage.setItem("expires_at", JSON.stringify(expiresAt.valueOf()));
-	}
+            )
+            .pipe(
+                map((authResult: any) => {
+                    this.setLocalStorage(authResult); // Llama a setLocalStorage con el resultado de autenticación
+                    console.log('from auth service ', authResult);
+                }),
+                catchError((error: HttpErrorResponse) => {
+                    //  Handle error here (show an error message)
+                    return throwError(error);
+                })
+            );
+    }
 
-	// By removing the token from localStorage, we have essentially "lost" our
-	// JWT in space and will need to re-authenticate with the Express app to get
-	// another one.
-	logout() {
-		localStorage.removeItem("id_token");
-		localStorage.removeItem("expires_at");
-	}
+    private setLocalStorage(authResult: any) {
+        // Takes the JWT expiresIn value and add that number of seconds
+        // to the current "moment" in time to get an expiry date
+        const expiresAt = moment().add(authResult.expiresIn, "second");
 
-	// Returns true as long as the current time is less than the expiry date
-	public isLoggedIn() {
-		return moment().isBefore(this.getExpiration());
-	}
+        // Stores our JWT token and its expiry date in localStorage
+        localStorage.setItem("id_token", authResult.idToken);
+        localStorage.setItem("expires_at", JSON.stringify(expiresAt.valueOf()));
+    }
 
-	isLoggedOut() {
-		return !this.isLoggedIn();
-	}
+    // By removing the token from localStorage, we have essentially "lost" our
+    // JWT in space and will need to re-authenticate with the Express app to get
+    // another one.
+    logout() {
+        localStorage.removeItem("id_token");
+        localStorage.removeItem("expires_at");
+    }
 
-	getExpiration() {
-		const expiration = localStorage.getItem("expires_at");
-		const expiresAt = expiration != null ? JSON.parse(expiration) : "";
-		return moment(expiresAt);
-	}
+    // Returns true as long as the current time is less than the expiry date
+    public isLoggedIn() {
+        return moment().isBefore(this.getExpiration());
+    }
+
+    isLoggedOut() {
+        return !this.isLoggedIn();
+    }
+
+    getExpiration() {
+        const expiration = localStorage.getItem("expires_at");
+        const expiresAt = expiration != null ? JSON.parse(expiration) : "";
+        return moment(expiresAt);
+    }
+    
 }
