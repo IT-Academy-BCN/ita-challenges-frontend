ab6dca9a25bc8eb273064e1f0b2926d37037835c
valerioprds <v.paradiso89@gmail.com>
2023-11-23 10:44:33 +0100

    changed environments api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   appVersion: require('../../package.json').version,
   BACKEND_ITA_CHALLENGE_BASE_URL: '',
-  BACKEND_ITA_WIKI_BASE_URL: 'https://dev.api.itadirectory.eurecatacademy.org/api/v1',
+  BACKEND_ITA_WIKI_BASE_URL: 'https://dev.sso.itawiki.eurecatacademy.org/api/v1',
   BACKEND_ALL_CHALLENGES: '/challenges',
   BACKEND_REGISTER: '/auth/register',
   AUTHORIZATION: 'Authorization',
