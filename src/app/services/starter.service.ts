818b269a179872209268d958eaa7ae70d68fa514
Lorena Ponsico <lorena.poca@gmail.com>
2023-11-30 11:30:36 +0100

    Change definitive endpoint starter service

--- src/app/services/starter.service.ts
+++ src/app/services/starter.service.ts
@@ -1,7 +1,7 @@
-import {Injectable} from "@angular/core";
-import {Observable} from "rxjs";
-//import {environment} from "../../environments/environment";
-import {HttpClient} from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { environment } from "../../environments/environment";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
     providedIn: 'root'
@@ -11,26 +11,23 @@ export class StarterService {
 
     constructor(private http: HttpClient) { }
 
-    getAllChallenges(page: number, pageSize: number):Observable<Object>{
-        /*        return this.http.get(`${environment.BACKEND_BASE_URL}${environment.BACKEND_ALL_CHALLENGES}`,
-                    {
-                        headers: {
-                            'Content-Type': 'application/dummy'
-                        }
-                    });*/
-
+    getAllChallenges(page: number, pageSize: number): Observable<Object> {
 
         const params = {
             page: page.toString(),
             pageSize: pageSize.toString()
-        }
-        return this.http.get('../assets/dummy/data-challenge.json',
+        };
+
+        console.log(params)
+
+        return this.http.get(`${environment.BACKEND_ITA_CHALLENGE_BASE_URL}${environment.BACKEND_ALL_CHALLENGES}`,
             {
                 params,
                 headers: {
-                    'Content-Type': 'application/dummy'
+                    'Content-Type': 'application/json'
                 }
             });
+
     }
 
 }
