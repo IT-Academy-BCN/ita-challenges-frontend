92c63117cd9090ccb7453f9c79538639bc6522af
Lorena Ponsico <lorena.poca@gmail.com>
2023-12-20 11:45:53 +0100

    service all challenges

--- src/app/services/challenge.service.ts
+++ src/app/services/challenge.service.ts
@@ -14,7 +14,7 @@ export class ChallengeService {
   
     getChallengeById(id: string):Observable<Object>{
 
-        const url = `${environment.BACKEND_ITA_CHALLENGE_BASE_URL}${environment.BACKEND_CHALLENGES_BY_ID}${id}`
+        const url = `${environment.BACKEND_ITA_CHALLENGE_BASE_URL}${environment.BACKEND_ALL_CHALLENGES}${id}`
         console.log(url)
         
         return this.http.get(url, 
