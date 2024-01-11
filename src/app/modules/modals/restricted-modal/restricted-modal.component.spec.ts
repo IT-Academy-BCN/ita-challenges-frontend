bc224ead15acb48ef99d8bedf1f5ed71d5cf59dd
valerioprds <v.paradiso89@gmail.com>
2024-01-11 11:20:34 +0100

    testing restricted modal now completed

--- src/app/modules/modals/restricted-modal/restricted-modal.component.spec.ts
+++ src/app/modules/modals/restricted-modal/restricted-modal.component.spec.ts
@@ -1,21 +1,66 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { RestrictedModalComponent } from "./restricted-modal.component";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { Router } from "@angular/router";
+import { RegisterModalComponent } from "../register-modal/register-modal.component";
+import { LoginModalComponent } from "../login-modal/login-modal.component";
 
-import { RestrictedModalComponent } from './restricted-modal.component';
+describe("RestrictedModalComponent", () => {
+	let component: RestrictedModalComponent;
+	let fixture: ComponentFixture<RestrictedModalComponent>;
+	let modalService: NgbModal;
+	let router: Router;
 
-describe('RestrictedModalComponent', () => {
-  let component: RestrictedModalComponent;
-  let fixture: ComponentFixture<RestrictedModalComponent>;
+	beforeEach(() => {
+		// Crear versiones simuladas de NgbModal y Router
+		const modalServiceMock = {
+			dismissAll: jest.fn(),
+			open: jest.fn(),
+		};
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [RestrictedModalComponent]
-    });
-    fixture = TestBed.createComponent(RestrictedModalComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		const routerMock = {
+			navigateByUrl: jest.fn(),
+		};
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+		TestBed.configureTestingModule({
+			declarations: [RestrictedModalComponent],
+			providers: [
+				{ provide: NgbModal, useValue: modalServiceMock },
+				{ provide: Router, useValue: routerMock },
+			],
+		});
+
+		fixture = TestBed.createComponent(RestrictedModalComponent);
+		component = fixture.componentInstance;
+		modalService = TestBed.inject(NgbModal);
+		router = TestBed.inject(Router);
+		fixture.detectChanges();
+	});
+
+	it("should create", () => {
+		expect(component).toBeTruthy();
+	});
+
+	it("should close modal when closeModal is called", () => {
+		component.closeModal();
+		expect(modalService.dismissAll).toHaveBeenCalled();
+	});
+
+	it("should close and open login modal when openLoginModal is called", () => {
+		component.openLoginModal();
+		expect(modalService.dismissAll).toHaveBeenCalled();
+		expect(modalService.open).toHaveBeenCalledWith(LoginModalComponent, {
+			centered: true,
+			size: "lg",
+		});
+	});
+
+	it("should close and open register modal when openRegisterModal is called", () => {
+		component.openRegisterModal();
+		expect(modalService.dismissAll).toHaveBeenCalled();
+		expect(modalService.open).toHaveBeenCalledWith(RegisterModalComponent, {
+			centered: true,
+			size: "lg",
+		});
+	});
 });
