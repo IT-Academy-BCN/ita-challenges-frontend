97d522d8a821aa040f69c27f9b6f5591c3b2a78d
laradelrio <lara.delrio333@gmail.com>
2024-03-11 09:43:27 +0100

    create post register modal

---
+++ src/app/modules/modals/post-register-modal/post-register-modal.component.spec.ts
@@ -0,0 +1,7 @@
+import { PostRegisterModalComponent } from './post-register-modal.component'
+
+describe('PostRegisterModalComponent', () => {
+  it('should mount', () => {
+    cy.mount(PostRegisterModalComponent)
+  })
+})
---
+++ src/app/modules/modals/post-register-modal/post-register-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-register-modal',
+  templateUrl: './post-register-modal.component.html',
+  styleUrl: './post-register-modal.component.scss'
+})
+export class PostRegisterModalComponent {
+
+}
