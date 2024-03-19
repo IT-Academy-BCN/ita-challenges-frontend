e5557fe948c8cef1edc7e7ad699953a44eec52c2
laradelrio <lara.delrio333@gmail.com>
2024-03-19 09:34:50 +0100

    custom loader test suite passed

--- src/app/modules/custom-loader/custom-loader.spec.ts
+++ src/app/modules/custom-loader/custom-loader.spec.ts
@@ -1,7 +1,24 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { CustomLoader } from './custom-loader'
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-// describe('CustomLoaderComponent', () => {
-//   it('should mount', () => {
-//     cy.mount(CustomLoader)
-//   })
-// })
+describe('CustomLoader', () => {
+    // let httpClientMock: HttpTestingController;
+    let component: CustomLoader;
+    let fixture: ComponentFixture<CustomLoader>;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({ // set up the testing module with required dependencies.
+            imports: [HttpClientTestingModule]
+        });
+        fixture = TestBed.createComponent(CustomLoader);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create login component correctly', (done) => {
+        expect(component).toBeTruthy();
+        done();
+    });
+    
+})
