cdfa83d8f61b7422d89f2b9f61ac6c59e574bb71
Jonatan Vicente <it@jonatanvicente.com>
2024-04-16 12:51:49 +0200

    Fixed register.modal.component.spec.ts
    Fixed resources.service.spec.ts to pass tests
    
    Fixed some tests - auth.service
    
    Injecting dependencies
    
    Added eslintignore

--- src/app/services/starter.service.ts
+++ src/app/services/starter.service.ts
@@ -1,30 +1,33 @@
-import { Injectable, inject } from '@angular/core'
-import { type Observable } from 'rxjs'
-import { environment } from '../../environments/environment'
-import { HttpClient, HttpHeaders } from '@angular/common/http'
-import { type Challenge } from '../models/challenge.model'
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { environment } from "../../environments/environment";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class StarterService {
-  private readonly http = inject(HttpClient)
 
-  getAllChallenges (page: number, pageSize: number): Observable<Challenge> {
-    // const params = {
-    //   page: page.toString(),
-    //   pageSize: pageSize.toString()
-    // }
+  constructor(private http: HttpClient) { }
+
+  getAllChallenges(page: number, pageSize: number): Observable<Object> {
+
+    const params = {
+      page: page.toString(),
+      pageSize: pageSize.toString()
+    };
 
     const headers = new HttpHeaders({
-      'Content-Type': 'application/json'
+      'Content-Type' : 'application/json'
     })
 
-    return this.http.get<Challenge>(`${environment.BACKEND_ITA_CHALLENGE_BASE_URL}${environment.BACKEND_ALL_CHALLENGES_URL}`,
-      {
-        // params,
-        headers
-      })
+    return this.http.get(`${environment.BACKEND_ITA_CHALLENGE_BASE_URL}${environment.BACKEND_ALL_CHALLENGES_URL}`,
+        {
+          //params,
+          headers
+        });
+
   }
+
 }
