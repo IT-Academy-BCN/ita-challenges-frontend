c046cb453b6deeb9c94e4341d28a4fbb9991c4c8
Samir Comas Moral <samir.comas1@gmail.com>
2024-04-17 12:56:53 +0200

    fix

--- src/app/services/resources.service.ts
+++ src/app/services/resources.service.ts
@@ -1,5 +1,5 @@
-import { type HttpClient } from '@angular/common/http'
-import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { Inject, Injectable } from '@angular/core'
 import { type Observable } from 'rxjs'
 import { environment } from 'src/environments/environment'
 import { type ResourceResponse } from '../models/resource.interface'
@@ -8,8 +8,9 @@ import { type ResourceResponse } from '../models/resource.interface'
   providedIn: 'root'
 })
 export class ResourcesService {
-  private readonly http = inject(HttpClient)
-
+  
+  // private readonly http = inject(HttpClient)
+  constructor(@Inject(HttpClient) private readonly http: HttpClient) { }
   getResources (): Observable<ResourceResponse> {
     // todo: need change the env api request.
     return this.http.get<ResourceResponse>(`${environment.BACKEND_SSO_RESOURCES}`,
