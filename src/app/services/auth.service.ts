ec644f829f452414b2c86f538530778c6d0f6fe7
Elisenda-LV <elisenda.lv@gmail.com>
2024-04-22 20:40:28 +0200

    Implement isUserLoggedIn()

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -218,17 +218,17 @@ export class AuthService {
   }
 
   /* Check if the user is  Logged in*/
-  public async isUserLoggedIn() { //TODO: neec tokenService first
-    // let isUserLoggedIn: boolean = false;
-    // let authToken = this.cookieService.get('authToken');
-    // let authTokenValid = await this.checkToken(authToken);
-    // if (authTokenValid) {
-    // 	isUserLoggedIn = true;
-    // } else {
-    // 	let refreshToken = this.cookieService.get('authToken');
-    // 	isUserLoggedIn = await this.checkToken(refreshToken);
-    // }
-    // return isUserLoggedIn;
+  public async isUserLoggedIn() { 
+    let isUserLoggedIn: boolean = false;
+    let authToken = this.cookieService.get('authToken');
+    let authTokenValid = await this.checkToken(authToken);
+      if (authTokenValid) {
+       	isUserLoggedIn = true;
+      } else {
+        let refreshToken = this.cookieService.get('refreshToken');
+        isUserLoggedIn = await this.checkToken(refreshToken);
+      }
+    return isUserLoggedIn;
   }
 
   /* return if token valid */
