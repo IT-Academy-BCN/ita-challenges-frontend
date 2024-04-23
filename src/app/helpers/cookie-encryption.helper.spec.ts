7537977d0bb1a8912dbe32389c2efc51603417db
Elisenda-LV <elisenda.lv@gmail.com>
2024-04-23 18:19:51 +0200

    testint esLint ok

--- src/app/helpers/cookie-encryption.helper.spec.ts
+++ src/app/helpers/cookie-encryption.helper.spec.ts
@@ -1,79 +1,75 @@
-import { TestBed } from '@angular/core/testing';
-import { CookieEncryptionHelper } from './cookie-encryption.helper';
-import { CookieService } from 'ngx-cookie-service';
-import { RegisterModalComponent } from 'src/app/modules/modals/register-modal/register-modal.component';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing'
+import { CookieEncryptionHelper } from './cookie-encryption.helper'
+import { CookieService } from 'ngx-cookie-service'
+import { RegisterModalComponent } from 'src/app/modules/modals/register-modal/register-modal.component'
+import { HttpClientTestingModule } from '@angular/common/http/testing'
 
 /**
  * Pruebas para CookieEncryptionService.
  */
 describe('CookieEncryptionService', () => {
-  let helper: CookieEncryptionHelper;
-  let mockCookieService: any;
+  let helper: CookieEncryptionHelper
+  let mockCookieService: any
 
-  beforeEach(async() => {
+  beforeEach(async () => {
     /**
       * Crea un objeto con funciones simuladas para cada método en CookieService
       */
     mockCookieService = {
       get: jest.fn(),
       set: jest.fn(),
-      delete: jest.fn(),
-    };
+      delete: jest.fn()
+    }
 
     await TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      declarations: [ RegisterModalComponent ],
+      declarations: [RegisterModalComponent],
       providers: [
         { provide: CookieService, useValue: mockCookieService },
-        CookieEncryptionHelper,
-      ],
+        CookieEncryptionHelper
+      ]
     })
-    .compileComponents();
-  
+      .compileComponents()
+
     /**
       * Crea una nueva instancia del servicio con el servicio de cookies simulado
       */
-    //service = new CookieEncryptionService(mockCookieService);
-    helper = TestBed.inject(CookieEncryptionHelper);
-
-  });
+    // service = new CookieEncryptionService(mockCookieService);
+    helper = TestBed.inject(CookieEncryptionHelper)
+  })
 
   /**
    * Prueba que el servicio se crea correctamente.
    */
   it('should be created', () => {
-    expect(helper).toBeTruthy();
-  });
-
+    expect(helper).toBeTruthy()
+  })
 
   /**
    * Prueba que el servicio encripta correctamente un valor.
    */
   it('should encrypt value', () => {
-    /*const result = helper.encryptValue('test');
-    expect(result).toEqual('encryptedValuePartencryptedValueFinal');*/
+    /* const result = helper.encryptValue('test');
+    expect(result).toEqual('encryptedValuePartencryptedValueFinal'); */
 
-    expect(true).toBeTruthy();
-  });
+    expect(true).toBeTruthy()
+  })
 
   /**
    * Prueba que el servicio establece correctamente una cookie encriptada.
    */
   it('should set encrypted cookie', () => {
-   /* helper.setEncryptedCookie('test', 'value');
+    /* helper.setEncryptedCookie('test', 'value');
     expect(mockCookieService.set).toHaveBeenCalledWith('test', 'encryptedValuePartencryptedValueFinal');
-   */ expect(true).toBeTruthy();
-  });
+   */ expect(true).toBeTruthy()
+  })
 
   /**
    * Prueba que el servicio desencripta correctamente un valor.
    */
   it('should decrypt value', () => {
-    /*const result = helper.decryptValue('test');
-    expect(result).toEqual('decryptedValuePartdecryptedValueFinal');*/
-    expect(true).toBeTruthy();
-  }); 
-
-
-});
+    /* const result = helper.decryptValue('test');
+    expect(result).toEqual('decryptedValuePartdecryptedValueFinal'); */
+    expect(true).toBeTruthy()
+  })
+})
--- src/app/helpers/cookie-encryption.helper.ts
+++ src/app/helpers/cookie-encryption.helper.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@angular/core';
-import { CookieService } from 'ngx-cookie-service';
-import { environment } from 'src/environments/environment';
+import { Injectable, inject } from '@angular/core'
+import { CookieService } from 'ngx-cookie-service'
+import { environment } from 'src/environments/environment'
 
 @Injectable({
   providedIn: 'root'
@@ -10,28 +10,28 @@ import { environment } from 'src/environments/environment';
  * Utiliza API Web Crypto para encriptar y desencriptar cookies, disponible en navegador.
  */
 export class CookieEncryptionHelper {
+  private secretKey!: CryptoKey
+  private readonly cookieService = inject(CookieService)
 
-  private secretKey!: CryptoKey;
-
-  //generación de secret key
-  constructor(private cookieService: CookieService) {
-    const password = environment.SECRET_PASSWORD;
-    this.generateSecretKey(password).then(key => {
-      this.secretKey = key;
-    });
+  // generación de secret key
+  constructor () {
+    const password = environment.SECRET_PASSWORD
+    void this.generateSecretKey(password).then(key => {
+      this.secretKey = key
+    })
   }
 
-  private async generateSecretKey(password: string): Promise<CryptoKey> {
-    const encoder = new TextEncoder();
-    const passwordBuffer = encoder.encode(password);
+  private async generateSecretKey (password: string): Promise<CryptoKey> {
+    const encoder = new TextEncoder()
+    const passwordBuffer = encoder.encode(password)
     const baseKey = await window.crypto.subtle.importKey(
       'raw',
       passwordBuffer,
-      { name: 'PBKDF2' }, //encrypt algorithm PBKDF2
+      { name: 'PBKDF2' }, // encrypt algorithm PBKDF2
       false,
       ['deriveKey']
-    );
-    return window.crypto.subtle.deriveKey(
+    )
+    return await window.crypto.subtle.deriveKey(
       {
         name: 'PBKDF2',
         salt: passwordBuffer,
@@ -39,45 +39,38 @@ export class CookieEncryptionHelper {
         hash: 'SHA-256'
       },
       baseKey,
-      { name: 'AES-GCM', length: 256 },//encriptado / desencriptado de cookie con algoritmo AES-GCM
+      { name: 'AES-GCM', length: 256 }, // encriptado / desencriptado de cookie con algoritmo AES-GCM
       true,
       ['encrypt', 'decrypt']
-    );
+    )
   }
 
-  async setSecureCookie(name: string, value: string): Promise<void> {
-    const encoder = new TextEncoder();
-    const valueBuffer = encoder.encode(value);
-    const iv = window.crypto.getRandomValues(new Uint8Array(12));
+  async setSecureCookie (name: string, value: string): Promise<void> {
+    const encoder = new TextEncoder()
+    const valueBuffer = encoder.encode(value)
+    const iv = window.crypto.getRandomValues(new Uint8Array(12))
     const encryptedValueBuffer = await window.crypto.subtle.encrypt(
-      { name: 'AES-GCM', iv: iv },
+      { name: 'AES-GCM', iv },
       this.secretKey,
       valueBuffer
-    );
-    const encryptedValueArray = new Uint8Array(encryptedValueBuffer);
-    const encryptedValue = btoa(String.fromCharCode(...encryptedValueArray));
-    this.cookieService.set(name, encryptedValue);
+    )
+    const encryptedValueArray = new Uint8Array(encryptedValueBuffer)
+    const encryptedValue = btoa(String.fromCharCode(...encryptedValueArray))
+    this.cookieService.set(name, encryptedValue)
   }
 
-  async getSecureCookie(name: string): Promise<string> {
-    const encryptedValue = this.cookieService.get(name);
-    const encryptedValueArray = Uint8Array.from(atob(encryptedValue), c => c.charCodeAt(0));
-    const iv = encryptedValueArray.slice(0, 12);
-    const encryptedValueBuffer = encryptedValueArray.slice(12).buffer;
+  async getSecureCookie (name: string): Promise<string> {
+    const encryptedValue = this.cookieService.get(name)
+    const encryptedValueArray = Uint8Array.from(atob(encryptedValue), c => c.charCodeAt(0))
+    const iv = encryptedValueArray.slice(0, 12)
+    const encryptedValueBuffer = encryptedValueArray.slice(12).buffer
     const valueBuffer = await window.crypto.subtle.decrypt(
-      { name: 'AES-GCM', iv: iv },
+      { name: 'AES-GCM', iv },
       this.secretKey,
       encryptedValueBuffer
-    );
-    const decoder = new TextDecoder();
-    const originalValue = decoder.decode(valueBuffer);
-    return originalValue;
+    )
+    const decoder = new TextDecoder()
+    const originalValue = decoder.decode(valueBuffer)
+    return originalValue
   }
 }
-
-
-
-
-
-
-
