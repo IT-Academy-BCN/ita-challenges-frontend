1d9529871ed106a360af93f40d55d9a5a70dc577
Samir Comas Moral <samir.comas1@gmail.com>
2024-04-24 10:25:47 +0200

    add format date function on resource-card.component.ts

--- src/app/shared/components/resource-card/resource-card.component.ts
+++ src/app/shared/components/resource-card/resource-card.component.ts
@@ -2,16 +2,18 @@ import { type ResourceResponse } from '../../../models/resource.interface'
 import { Component, Input, inject, type OnInit } from '@angular/core'
 import { ChallengeService } from '../../../services/challenge.service'
 import { ResourcesService } from 'src/app/services/resources.service'
+import { DatePipe } from '@angular/common'
 
 @Component({
   selector: 'app-resource-card',
   templateUrl: './resource-card.component.html',
   styleUrls: ['./resource-card.component.scss'],
-  providers: []
+  providers: [DatePipe]
 })
 export class ResourceCardComponent implements OnInit {
   private readonly challengeService = inject(ChallengeService)
   private readonly resourcesService = inject(ResourcesService)
+  private readonly datePipe = inject(DatePipe)
 
   @Input() inputResources: any = ''
   public resources: ResourceResponse[] = []
@@ -25,5 +27,12 @@ export class ResourceCardComponent implements OnInit {
         console.error(error)
       }
     })
+  }
+
+  formatDate (date: string | null): string | null {
+    if (date !== null) {
+      return this.datePipe.transform(date, 'MMM d, y')
+    }
+    return ''
   }
 }
