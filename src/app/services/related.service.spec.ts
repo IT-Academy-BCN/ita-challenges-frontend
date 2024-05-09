1c2c04d2fd6bd14f7107644045b0bc6c68c8ca74
Cesc <francesc9000@gmail.com>
2024-05-09 11:40:20 +0200

    feature#215f related.service testing

--- src/app/services/related.service.spec.ts
+++ src/app/services/related.service.spec.ts
@@ -1,48 +1,154 @@
-import { RelatedService } from "./related.service";
-import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
-import { HttpClient } from "@angular/common/http";
-import { environment } from "src/environments/environment";
-import { TestBed } from "@angular/core/testing";
+import { TestBed } from '@angular/core/testing'
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
+import { environment } from 'src/environments/environment'
+import { RelatedService } from './related.service';
 
 describe('RelatedService', () => {
-
-  let service: RelatedService;
-  let httpClient: HttpClient;
-  let httpClientMock: HttpTestingController;
+  let relatedService: RelatedService
+  let httpClientMock: HttpTestingController
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule]
-    });
-    httpClient = TestBed.inject(HttpClient);
-    httpClientMock = TestBed.inject(HttpTestingController);
-    service = new RelatedService(httpClient);
-  });
+    TestBed.configureTestingModule({ // set up the testing module with required dependencies.
+      imports: [HttpClientTestingModule],
+      providers: [RelatedService]
+    })
 
-  it('Should return null when related challenges are NOT FOUND', (done) => {
-    httpClientMock.expectOne(`${environment.BACKEND_ITA_CHALLENGE_BASE_URL}${environment.BACKEND_ALL_CHALLENGES_URL}/challengeId/related`).flush(null);
+    httpClientMock = TestBed.inject(HttpTestingController)
+    relatedService = TestBed.inject(RelatedService)
+  })
 
-    const challenges = service.getRelatedChallenges('challengeId');
+  afterEach(() => {
+    httpClientMock.verify()
+  })
 
-    expect(challenges).toBeDefined();
-    expect(challenges).toBe(null);
+  it('should be created', () => {
+    expect(relatedService).toBeTruthy()
+  })
 
-    done();
-  });
+  it('should return related challenges response correctly', (done) => {
+    const relatedId = 'f6e0f877-9560-4e68-bab6-7dd5f16b46a5'
+    const responseMock = {
+      "id_challenge": "f6e0f877-9560-4e68-bab6-7dd5f16b46a5",
+      "challenge_title": {
+          "es": "Buscando los extremos",
+          "en": "Searching for extremes",
+          "ca": "Buscant els extrems"
+      },
+      "level": "EASY",
+      "creation_date": "1986-02-01",
+      "detail": {
+          "description": {
+              "es": "En esta pequeña tarea, se te proporciona una cadena de números separados por espacios, y debes devolver el número más alto y el más bajo.",
+              "ca": "En aquesta petita tasca set donara una string de nombre separats per espais, hauras de retornar el major i menor d'aquests nombres.",
+              "en": "In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number."
+          },
+          "examples": [
+              {
+                  "id_example": "aef3ebe5-1a11-4b32-9f6e-98d27e2be22d",
+                  "example_text": {
+                      "es": "Orden descendente",
+                      "ca": "Ordre descendent",
+                      "en": "Descending Order"
+                  }
+              },
+              {
+                  "id_example": "8398539e-7b48-4a1e-9b8b-136f711c3dc9",
+                  "example_text": {
+                      "es": "Orden descendente",
+                      "ca": "Ordre descendent",
+                      "en": "Descending Order"
+                  }
+              },
+              {
+                  "id_example": "1e6af8d0-ccbf-4ebf-90ae-8e9b098a7a15",
+                  "example_text": {
+                      "es": "Orden descendente",
+                      "ca": "Ordre descendent",
+                      "en": "Descending Order"
+                  }
+              }
+          ],
+          "note": null
+      },
+      "languages": [
+          {
+              "id_language": "660e1b18-0c0a-4262-a28a-85de9df6ac5f",
+              "language_name": "Java"
+          }
+      ],
+      "solutions": [
+          "0864463e-eb7c-4bb3-b8bc-766d71ab38b5"
+      ],
+      "testingValues": [
+          {
+              "in_param": [
+                  1,
+                  2,
+                  3,
+                  4,
+                  5
+              ],
+              "out_param": [
+                  5,
+                  1
+              ]
+          },
+          {
+              "in_param": [
+                  1,
+                  2,
+                  -3,
+                  4,
+                  5
+              ],
+              "out_param": [
+                  5,
+                  -3
+              ]
+          },
+          {
+              "in_param": [
+                  1,
+                  9,
+                  3,
+                  4,
+                  -5
+              ],
+              "out_param": [
+                  9,
+                  -5
+              ]
+          }
+      ]
+  }
 
-  it('Should return the related challenges when they ARE FOUND', (done) => {
-    const mockChallenges = [
-      { challengeId: '2bfc1a9e-30e3-40b2-9e97-8db7c5a4e9e4', name: 'mockName1' },
-    ];
+    relatedService.getRelatedChallenges(relatedId).subscribe(response => {
+      expect(response).toEqual(responseMock)
+      done()
+    })
 
-    httpClientMock.expectOne(`${environment.BACKEND_ITA_CHALLENGE_BASE_URL}${environment.BACKEND_ALL_CHALLENGES_URL}/${challengeId}/related`).flush(mockChallenges);
+    const req = httpClientMock.expectOne(`${environment.BACKEND_ITA_CHALLENGE_BASE_URL}${environment.BACKEND_ALL_CHALLENGES_URL}/${relatedId}/related`)
+    expect(req.request.method).toEqual('GET')
 
-    const challenges = service.getRelatedChallenges('2bfc1a9e-30e3-40b2-9e97-8db7c5a4e9e4');
+    req.flush(responseMock)
+  })
 
-    expect(challenges).toBeDefined();
-    expect(challenges).toBe(mockChallenges);
+  it('should return related challenges response error', (done) => {
+    const relatedId = 'errorId'
+    const errorMessage = 'Challenge with id errorId not found'
+    const status = 500
 
-    done();
-  });
+    relatedService.getRelatedChallenges(relatedId).subscribe({
+      error: err => {
+        expect(err.status).toEqual(status)
+        expect(err.error).toEqual(errorMessage)
+        done()
+      }
+    })
 
-});
+    const req = httpClientMock.expectOne(`${environment.BACKEND_ITA_CHALLENGE_BASE_URL}${environment.BACKEND_ALL_CHALLENGES_URL}/${relatedId}/related`)
+    expect(req.request.method).toEqual('GET')
+
+    req.flush(errorMessage, { status, statusText: 'Internal Server Error' })
+  })
+})
