7cf683f1c7e21c700f986c28a643d5b1610e5620
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2024-06-03 12:47:11 +0200

    Añadidos pesos de fuente adicionales para Poppins

--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core'
+import { Component, inject } from '@angular/core'
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap'
+import { LoginModalComponent } from './../../../modules/modals/login-modal/login-modal.component'
+import { TranslateService } from '@ngx-translate/core'
 
 @Component({
   selector: 'app-header',
@@ -6,5 +9,21 @@ import { Component } from '@angular/core'
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  private readonly modalService = inject(NgbModal)
+  private readonly translate = inject(TranslateService)
+  constructor () {
+    this.translate.addLangs(['en', 'es', 'ca'])
+    this.translate.setDefaultLang('ca')
+    this.translate.use('ca')
+  }
 
+  openLoginModal (): void {
+    this.modalService.open(LoginModalComponent, { centered: true, size: 'lg' })
+  }
+
+  changeLanguage (event: Event): void {
+    const selectElement = event.target as HTMLSelectElement
+    const language = selectElement.value
+    this.translate.use(language)
+  }
 }
