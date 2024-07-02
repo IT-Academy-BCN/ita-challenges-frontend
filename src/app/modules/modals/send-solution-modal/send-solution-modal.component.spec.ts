64946fece24c69d80a3196ef5aab1633a8e1acde
evarkitecta <evarkitecta@gmail.com>
2024-07-02 11:29:57 +0200

    i18n of send-solution-modal completed

--- src/app/modules/modals/send-solution-modal/send-solution-modal.component.spec.ts
+++ src/app/modules/modals/send-solution-modal/send-solution-modal.component.spec.ts
@@ -1,17 +1,37 @@
 import { type ComponentFixture, TestBed } from '@angular/core/testing'
 import { SendSolutionModalComponent } from './send-solution-modal.component'
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
+import { NgbModal, NgbModule } from '@ng-bootstrap/ng-bootstrap'
+import { TranslateModule } from '@ngx-translate/core'
+import { Router } from '@angular/router'
 
 describe('SendSolutionModalComponent', () => {
   let component: SendSolutionModalComponent
   let fixture: ComponentFixture<SendSolutionModalComponent>
+  let modalServiceMock: any
+  let routerMock: any
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    // Crear versiones simuladas de NgbModal y Router
+    modalServiceMock = {
+      dismissAll: jest.fn(),
+      open: jest.fn()
+    }
+
+    routerMock = {
+      navigateByUrl: jest.fn()
+    }
+
+    await TestBed.configureTestingModule({
       declarations: [SendSolutionModalComponent],
-      imports: [],
-      providers: [provideHttpClient(withInterceptorsFromDi())]
-    })
+      imports: [NgbModule, TranslateModule.forRoot()],
+      providers: [
+        provideHttpClient(withInterceptorsFromDi()),
+        { provide: NgbModal, useValue: modalServiceMock },
+        { provide: Router, useValue: routerMock }
+      ]
+    }).compileComponents()
+
     fixture = TestBed.createComponent(SendSolutionModalComponent)
     component = fixture.componentInstance
     fixture.detectChanges()
