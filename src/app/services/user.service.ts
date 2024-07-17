647ba3dba6de3c44f42bd2fb1c6a0840be34f0ee
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2024-07-17 09:39:15 +0200

    Refactor: Eliminar dependencia de UserService en AuthService y mover lógica de estado de usuario a UserService

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,10 +1,26 @@
-import { Injectable } from '@angular/core'
+import { Inject, Injectable } from '@angular/core'
+import { AuthService } from './auth.service'
+import { type User } from '../models/user.model'
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  userRegistered: boolean = false
-  userLoggedIn: boolean = false
-  userSentASolution: boolean = false
+  public userRegistered: boolean = false
+  public userLoggedIn: boolean = false
+  public userSentASolution: boolean = false
+
+  constructor (@Inject(AuthService) private readonly authService: AuthService) { }
+
+  public async login (user: User): Promise<void> {
+    await this.authService.login(user)
+    this.userLoggedIn = true
+    console.log(`userLoggedIn: ${this.userLoggedIn}`)
+  }
+
+  public logout (): void {
+    this.authService.logout()
+    this.userLoggedIn = false
+    console.log(`userLoggedIn: ${this.userLoggedIn}`)
+  }
 }
