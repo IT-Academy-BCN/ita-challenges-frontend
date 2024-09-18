ce8986fea4ec1b99492623fd3d3e3587c8852ac5
Eloi Ginesti <ginesti1979@gmail.com>
2024-09-18 16:15:31 +0200

    ejercicio_terminado

--- src/app/modules/modals/filters-modal/filters-modal.component.ts
+++ src/app/modules/modals/filters-modal/filters-modal.component.ts
@@ -1,9 +1,9 @@
-import { Component, Output, ViewChild, EventEmitter, inject, type TemplateRef } from '@angular/core';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { FormBuilder } from '@angular/forms';
-import { type FilterChallenge } from 'src/app/models/filter-challenge.model';
-import { ChallengeService } from 'src/app/services/challenge.service';
-import { type Language } from 'src/app/models/language.model';
+import { Component, Output, ViewChild, EventEmitter, inject, type TemplateRef } from '@angular/core'
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap'
+import { FormBuilder } from '@angular/forms'
+import { type FilterChallenge } from 'src/app/models/filter-challenge.model'
+import { ChallengeService } from 'src/app/services/challenge.service'
+import { type Language } from 'src/app/models/language.model'
 
 @Component({
   selector: 'app-filters-modal',
@@ -11,15 +11,12 @@ import { type Language } from 'src/app/models/language.model';
   styleUrls: ['./filters-modal.component.scss']
 })
 export class FiltersModalComponent {
-  @Output() filtersSelected = new EventEmitter<FilterChallenge>();
-  @ViewChild('modal') private readonly modalContent!: TemplateRef<FiltersModalComponent>;
-  
-  private readonly modalService = inject(NgbModal);
-  private readonly formBuilder = inject(FormBuilder);
-  private readonly challengeService = inject(ChallengeService);
-
-
-  public languages: Record<string, string> = {};
+  @Output() filtersSelected = new EventEmitter<FilterChallenge>()
+  @ViewChild('modal') private readonly modalContent!: TemplateRef<FiltersModalComponent>
+  private readonly modalService = inject(NgbModal)
+  private readonly formBuilder = inject(FormBuilder)
+  private readonly challengeService = inject(ChallengeService)
+  public languages: Record<string, string> = {}
   formGroup = this.formBuilder.group({
     languages: this.formBuilder.nonNullable.group({
       javascript: false,
@@ -37,70 +34,66 @@ export class FiltersModalComponent {
       started: false,
       finished: false
     })
-  });
+  })
 
-
-  applyFilters(): void {
+  applyFilters (): void {
     this.challengeService.getAllLanguages().subscribe((res: any) => {
-      if (res.results) {
+      if (res.results !== undefined) {
         this.languages = res.results.reduce((acc: Record<string, string>, result: Language) => {
-          acc[result.language_name.toLowerCase()] = result.id_language;
-          return acc;
-        }, {});
+          acc[result.language_name.toLowerCase()] = result.id_language
+          return acc
+        }, {})
 
         // Procesar filtros después de cargar los idiomas
-        this.processFilters();
+        this.processFilters()
       }
-    });
+    })
   }
 
-  private processFilters(): void {
-    const formValue = this.formGroup.value;
-    const filters: FilterChallenge = { languages: [], levels: [], progress: [] };
+  private processFilters (): void {
+    const formValue = this.formGroup.value
+    const filters: FilterChallenge = { languages: [], levels: [], progress: [] }
 
     // Manejo de languages
-    if (formValue.languages) {
+    if (formValue.languages !== null && formValue.languages !== undefined) {
       Object.entries(formValue.languages).forEach(([key, val]) => {
         if (val) {
-          const idLanguage = this.languages[key];
-          if (idLanguage) {
-            filters.languages.push(idLanguage);
+          const idLanguage = this.languages[key]
+          if (idLanguage !== '') {
+            filters.languages.push(idLanguage)
           }
         }
-      });
+      })
     }
 
     // Manejo de levels
-    if (formValue.levels) {
+    if (formValue.levels !== null && formValue.levels !== undefined) {
       Object.entries(formValue.levels).forEach(([key, val]) => {
         if (val) {
-          filters.levels.push(key.toUpperCase());
+          filters.levels.push(key.toUpperCase())
         }
-      });
+      })
     }
 
     // Manejo de progress
-    if (formValue.progress) {
+    if (formValue.progress !== null && formValue.progress !== undefined) {
       Object.entries(formValue.progress).forEach(([key, val], i) => {
         if (val) {
-          filters.progress.push(i + 1);
+          filters.progress.push(i + 1)
         }
-      });
+      })
     }
 
-    console.log('Languages:', filters.languages);
-    console.log('Levels:', filters.levels);
-    console.log('Progress:', filters.progress);
-    console.log('filters:', filters);
+    console.log('Languages:', filters.languages)
+    console.log('Levels:', filters.levels)
+    console.log('Progress:', filters.progress)
+    console.log('filters:', filters)
 
-    this.filtersSelected.emit(filters); 
-    this.modalService.dismissAll();
+    this.filtersSelected.emit(filters)
+    this.modalService.dismissAll()
   }
-  
-  open(): void {
-    this.modalService.open(this.modalContent, { size: 'lg' });
+
+  open (): void {
+    this.modalService.open(this.modalContent, { size: 'lg' })
   }
 }
-
-
-
