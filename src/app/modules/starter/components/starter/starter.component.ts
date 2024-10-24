f8b7dd873a5fa4844fc845e7990faa701d9e9ad2
Eloi Ginesti <ginesti1979@gmail.com>
2024-10-24 11:06:19 +0200

    starter1.0

--- src/app/modules/starter/components/starter/starter.component.ts
+++ src/app/modules/starter/components/starter/starter.component.ts
@@ -6,7 +6,8 @@ import { Challenge } from '../../../../models/challenge.model'
 import { environment } from '../../../../../environments/environment'
 import { type FiltersModalComponent } from 'src/app/modules/modals/filters-modal/filters-modal.component'
 import { TranslateService } from '@ngx-translate/core'
-
+/* import { RouteConfigLoadEnd } from '@angular/router'
+ */
 @Component({
   selector: 'app-starter',
   templateUrl: './starter.component.html',
@@ -15,7 +16,7 @@ import { TranslateService } from '@ngx-translate/core'
 })
 export class StarterComponent implements OnInit {
   @ViewChild('modal') private readonly modalContent!: FiltersModalComponent
-  @ViewChild('challenges') challengesContainer!: ElementRef
+  @ViewChild('challenge') challengesContainer!: ElementRef
 
   challenges: Challenge[] = []
   params$!: Subscription
@@ -31,18 +32,14 @@ export class StarterComponent implements OnInit {
 
   selectedSort: string = ''
   isAscending: boolean = false
-  isLoading: boolean = false
+  startIndex: number = 0
   constructor (
     @Inject(StarterService) private readonly starterService: StarterService,
     @Inject(TranslateService) readonly translate: TranslateService
-  ) {
-    /*    this.params$ = this.activatedRoute.params.subscribe(params => {
-
-    }) */
-  }
+  ) {}
 
   ngOnInit (): void {
-    this.getChallengesByPage(this.pageNumber)
+    this.getChallenge()
   }
 
   ngOnDestroy (): void {
@@ -50,30 +47,42 @@ export class StarterComponent implements OnInit {
     if (this.challengesSubs$ !== undefined) this.challengesSubs$.unsubscribe()
   }
 
+  getChallenge (): void {
+    this.starterService.getAllChallenges().subscribe({
+      next: (resp) => {
+        this.listChallenges = resp
+        console.log('Datos recibidos:', this.listChallenges)
+        this.getChallengesByPage(this.pageNumber)
+      },
+      error: (err) => {
+        console.error('Error al obtener los desafíos:', err)
+      }
+    })
+  }
+
   getChallengesByPage (page: number): void {
-    const getChallengeOffset = 8 * (page - 1)
-    console.log(getChallengeOffset)
     this.pageNumber = page
-    console.log(this.pageNumber)
-    if (this.filters.languages.length > 0 || this.filters.levels.length > 0 || this.filters.progress.length > 0) {
-      this.getChallengeFilters(this.filters)
+    const startIndex = (this.pageNumber - 1) * this.pageSize
+
+    if (Array.isArray(this.listChallenges) && this.listChallenges.length > 0) {
+      this.totalPages = Math.ceil(this.listChallenges.length / this.pageSize)
+
+      if (this.sortBy !== '') {
+        this.getAndSortChallenges(startIndex, this.listChallenges)
+      }
+
+      if (window.innerWidth < 768) {
+        // Para móviles, podrías querer ajustar la lógica según cómo manejas la paginación
+        this.challenges = this.listChallenges
+      } else {
+        // Para escritorio, muestra los desafíos según la paginación
+        this.challenges = this.listChallenges.slice(startIndex, startIndex + this.pageSize)
+      }
+
+      console.log('Desafíos en la página actual:', this.challenges)
     } else {
-      const challengesObservable = this.sortBy !== ''
-        ? this.starterService.getAllChallenges()
-        : this.starterService.getAllChallengesOffset(getChallengeOffset, this.pageSize)
-      console.log(challengesObservable)
-      this.challengesSubs$ = challengesObservable.subscribe(resp => {
-        if (this.sortBy !== '') {
-          this.getAndSortChallenges(getChallengeOffset, resp)
-          this.isLoading = false
-        } else {
-          this.listChallenges = resp
-          console.log(this.listChallenges)
-          this.totalPages = Math.ceil(22 / this.pageSize) // Cambiar 22 por el valor de challenge.count
-          this.isLoading = false
-        }
-        this.isLoading = false
-      })
+      console.warn('No hay desafíos disponibles.')
+      this.challenges = []
     }
   }
 
@@ -83,17 +92,21 @@ export class StarterComponent implements OnInit {
 
   private getAndSortChallenges (getChallengeOffset: number, resp: any): void {
     const respArray: Challenge[] = Array.isArray(resp) ? resp : [resp]
+
     const sortedChallenges$ = this.isAscending
-      ? this.starterService.orderBySortAscending(this.sortBy, respArray, getChallengeOffset, this.pageSize)
-      : this.starterService.orderBySortAsDescending(this.sortBy, respArray, getChallengeOffset, this.pageSize)
+      ? this.starterService.orderBySortAscending(this.sortBy, respArray, 0, respArray.length)
+      : this.starterService.orderBySortAsDescending(this.sortBy, respArray, 0, respArray.length)
 
     sortedChallenges$.subscribe(sortedResp => {
       this.listChallenges = sortedResp
-      this.totalPages = Math.ceil(respArray.length / this.pageSize)
+      console.log('Todos los desafíos ordenados:', this.listChallenges)
+      this.totalPages = Math.ceil(this.listChallenges.length / this.pageSize)
+      this.challenges = this.listChallenges.slice(getChallengeOffset, getChallengeOffset + this.pageSize)
+      console.log('Desafíos ordenados y en la página actual:', this.challenges)
     })
   }
 
-  getChallengeFilters (filters: FilterChallenge): void {
+  /*  getChallengeFilters (filters: FilterChallenge): void {
     if (this.filters !== filters) {
       this.pageNumber = 1
     }
@@ -115,7 +128,6 @@ export class StarterComponent implements OnInit {
                   orderBySortFunction(this.sortBy, filteredResp, getChallengeOffset, this.pageSize).subscribe(sortedResp => {
                     this.listChallenges = sortedResp
                     this.totalPages = Math.ceil(filteredResp.length / this.pageSize)
-                    this.isLoading = false
                   })
                 } else {
                   console.error('filteredResp no es un array de Challenge')
@@ -123,19 +135,17 @@ export class StarterComponent implements OnInit {
               } else {
                 this.listChallenges = filteredResp.slice(getChallengeOffset, getChallengeOffset + this.pageSize)
                 this.totalPages = Math.ceil(filteredResp.length / this.pageSize)
-                this.isLoading = false
               }
             })
         } else {
           this.listChallenges = resp
           this.totalPages = Math.ceil(22 / this.pageSize) // Cambiar 22 por el valor de challenge.count
-          this.isLoading = false
-        }
+        }ç
       })
     } else {
       this.getChallengesByPage(this.pageNumber)
     }
-  }
+  } */
 
   changeSort (newSort: string): void {
     this.sortBy = newSort
@@ -149,45 +159,6 @@ export class StarterComponent implements OnInit {
         this.getChallengesByPage(this.pageNumber)
         this.isAscending = true
       }
-    }
-  }
-
-  ngAfterViewInit (): void {
-    this.initScrollEvent()
-  }
-
-  onScroll (): void {
-    const scrollTop = this.challengesContainer.nativeElement.scrollTop
-    const containerHeight = this.challengesContainer.nativeElement.clientHeight
-    const scrollHeight = this.challengesContainer.nativeElement.scrollHeight
-
-    // Verificar si ya está cargando para evitar múltiples llamadas
-    if (this.isLoading) {
-      return
-    }
-    // Condición para cargar la siguiente página al llegar al final del contenedor
-    if (scrollTop + containerHeight >= scrollHeight) {
-      this.pageNumber++
-      this.isLoading = true // Activar bandera de carga
-      console.log(this.pageNumber)
-      this.getChallengesByPage(this.pageNumber)
-    }
-
-    // Condición para cargar la página anterior al llegar al principio del contenedor
-    if (scrollTop <= 0 && this.pageNumber > 1) {
-      this.pageNumber--
-      this.isLoading = true // Activar bandera de carga
-      console.log(this.pageNumber)
-      this.getChallengesByPage(this.pageNumber)
-    }
-  }
-
-  // Método para inicializar el evento de scroll
-  initScrollEvent (): void {
-    if (window.innerWidth < 768) {
-      this.challengesContainer.nativeElement.addEventListener('scroll', () => {
-        this.onScroll()
-      })
     }
   }
 }
