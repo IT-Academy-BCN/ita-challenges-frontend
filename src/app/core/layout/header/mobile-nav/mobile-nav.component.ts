056967b9f9b006b0da29e6838735b9d9f4b34e30
Fran Palmero <sweetydesigns@gmail.com>
2024-10-29 19:29:21 +0100

    Mobile Menu: Add content
    WIP

--- src/app/core/layout/header/mobile-nav/mobile-nav.component.ts
+++ src/app/core/layout/header/mobile-nav/mobile-nav.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core'
+import { Component, inject } from '@angular/core'
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap'
+import { LoginModalComponent } from './../../../../modules/modals/login-modal/login-modal.component'
+import { TranslateService } from '@ngx-translate/core'
 
 @Component({
   selector: 'app-mobile-nav',
@@ -6,5 +9,24 @@ import { Component } from '@angular/core'
   styleUrls: ['./mobile-nav.component.scss']
 })
 export class MobileNavComponent {
+  selectWidth = '69px'
+  private readonly modalService = inject(NgbModal)
+  private readonly translate = inject(TranslateService)
+  constructor () {
+    this.translate.addLangs(['en', 'es', 'ca'])
+    this.translate.setDefaultLang('ca')
+    this.translate.use('ca')
+  }
+
+  openLoginModal (): void {
+    this.modalService.open(LoginModalComponent, { centered: true, size: 'lg' })
+  }
+
+  changeLanguage (event: Event): void {
+    const selectElement = event.target as HTMLSelectElement
+    const language = selectElement.value
+    this.translate.use(language)
+    this.selectWidth = language === 'ca' ? '69px' : '57px'
+  }
 
 }
