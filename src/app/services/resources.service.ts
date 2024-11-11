5461ef999d4250db44a22962e4f69ecc54ec3671
Fran Palmero <sweetydesigns@gmail.com>
2024-11-11 10:44:14 +0100

    Feature#454: Config Management Refactoring
    - Refactor: import version directly from package.json instead of packageInfo.
    
    - Renamed `BACKEND_SSO_RESOURCES` to `BACKEND_ITA_WIKI_RESOURCES`
    
    - Update resource.service to match new `BACKEND_ITA_WIKI_RESOURCES` key.
    
    - Added in PRODUCTION and renamed `BACKEND_SSO_CATEGORIES_URL` to `BACKEND_ITA_WIKI_CATEGORIES` key.
    
    - Corrected `BACKEND_ITA_WIKI_BASE_URL` to match non-dev URL format.

--- src/app/services/resources.service.ts
+++ src/app/services/resources.service.ts
@@ -15,8 +15,7 @@ export class ResourcesService {
 
   constructor (@Inject(HttpClient) private readonly http: HttpClient) { }
   getResources (): Observable<ResourceResponse> {
-    // todo: need change the env api request.
-    return this.http.get<ResourceResponse>(environment.BACKEND_ITA_WIKI_BASE_URL.concat(environment.BACKEND_SSO_RESOURCES),
+    return this.http.get<ResourceResponse>(environment.BACKEND_ITA_WIKI_BASE_URL.concat(environment.BACKEND_ITA_WIKI_RESOURCES),
       {
         headers: {
           'Content-Type': 'application/json'
