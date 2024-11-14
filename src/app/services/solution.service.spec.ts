e842ab39a0b1e239ad41e9cb9808827bd3e93347
Alessandro De Pietri Tonelli <adepietri.it@gmail.com>
2024-11-14 13:34:10 +0100

    remove dummy from test, import from mock folder

--- src/app/services/solution.service.spec.ts
+++ src/app/services/solution.service.spec.ts
@@ -2,8 +2,11 @@ import { TestBed } from '@angular/core/testing'
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
 import { SolutionService } from './solution.service'
 import { environment } from 'src/environments/environment'
-import { type DataSolution } from '../models/data-solution.model'
-import { type UserSolution } from '../models/user-solution.interface'
+// import { type DataSolution } from '../models/data-solution.model'
+// import { type UserSolution } from '../models/user-solution.interface'
+import mockResponse from '../../mocks/solution/solution-sended.json'
+import mockData from '../../mocks/solution/data-solution.json'
+import mockUserSolution from '../../mocks/solution/user-solution.json'
 
 describe('SolutionService', () => {
   let service: SolutionService
@@ -46,25 +49,6 @@ describe('SolutionService', () => {
   it('should return all challenge solutions', (done) => {
     const testChallengeId = 'dcacb291-b4aa-4029-8e9b-284c8ca80296'
     const testLanguageId = '409c9fe8-74de-4db3-81a1-a55280cf92ef'
-    const mockData: DataSolution = {
-      offset: 0,
-      limit: 2,
-      count: 2,
-      results: [
-        {
-          id_solution: '1682b3e9-056a-45b7-a0e9-eaf1e11775ad',
-          solution_text: 'Sample solution text',
-          uuid_language: '409c9fe8-74de-4db3-81a1-a55280cf92ef',
-          uuid_challenge: 'dcacb291-b4aa-4029-8e9b-284c8ca80296'
-        },
-        {
-          id_solution: 'a7a789a9-2006-4b59-94bb-3afe0d1c161d',
-          solution_text: 'Another solution text',
-          uuid_language: '409c9fe8-74de-4db3-81a1-a55280cf92ef',
-          uuid_challenge: 'dcacb291-b4aa-4029-8e9b-284c8ca80296'
-        }
-      ]
-    }
 
     service.getAllChallengeSolutions(testChallengeId, testLanguageId).subscribe(data => {
       expect(data.results[0].id_solution).toEqual('1682b3e9-056a-45b7-a0e9-eaf1e11775ad')
@@ -80,24 +64,6 @@ describe('SolutionService', () => {
     const userId = 'user123'
     const challengeId = 'challenge123'
     const languageId = 'language123'
-    const mockUserSolution: UserSolution = {
-      offset: 0,
-      limit: 1,
-      count: 0,
-      results: [
-        {
-          id_challenge: '7fc6a737-dc36-4e1b-87f3-120d81c548aa',
-          language: '1e047ea2-b787-49e7-acea-d79e92be3909',
-          id_user: 'c3a92f9d-5d10-4f76-8c0b-6d884c549b1c',
-          solutions: [
-            {
-              uuid: 'dcacb291-b4aa-4029-8e9b-284c8ca80296',
-              solutionText: 'Sed dictum. Proin eget odio. Aliquam vulputate ullamcorper magna. Sed eu eros. Nam consequat dolor vitae dolor. Donec fringilla. Donec'
-            }
-          ]
-        }
-      ]
-    }
 
     service.getUserSolution(userId, challengeId, languageId).subscribe(data => {
       expect(data.results[0].solutions[0].uuid).toEqual('dcacb291-b4aa-4029-8e9b-284c8ca80296')
@@ -111,28 +77,6 @@ describe('SolutionService', () => {
 
   it('should fetch user solutions', (done) => {
     const userId = 'user123'
-    const mockResponse = {
-      challenges: [
-        {
-          uuid_challenge: 'challenge1',
-          solutions: [
-            {
-              uuid: 'solution1',
-              solutionText: 'Sample solution text 1'
-            }
-          ]
-        },
-        {
-          uuid_challenge: 'challenge2',
-          solutions: [
-            {
-              uuid: 'solution2',
-              solutionText: 'Sample solution text 2'
-            }
-          ]
-        }
-      ]
-    }
 
     service.fetchUserSolution(userId).subscribe((data) => {
       expect(data).toEqual(mockResponse)
