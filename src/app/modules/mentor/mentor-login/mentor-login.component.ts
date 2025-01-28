3b9aff23e2b4ac5233da381141241d1a26a1323d
Ivan <ielizalde.dev@gmail.com>
2025-01-28 22:17:50 +0100

    create new mentor-login component

---
+++ src/app/modules/mentor/mentor-login/mentor-login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-mentor-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './mentor-login.component.html',
+  styleUrl: './mentor-login.component.scss'
+})
+export class MentorLoginComponent {
+
+}
