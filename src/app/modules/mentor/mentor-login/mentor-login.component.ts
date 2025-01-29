069d31719cae6c203199d6326df84278372c4346
Ivan <ielizalde.dev@gmail.com>
2025-01-29 13:00:08 +0100

    implement loginWithGitHub function to retrive only the code

--- src/app/modules/mentor/mentor-login/mentor-login.component.ts
+++ src/app/modules/mentor/mentor-login/mentor-login.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core'
+// eslint-disable-next-line @typescript-eslint/consistent-type-imports
+import { Component, OnInit } from '@angular/core'
+// eslint-disable-next-line @typescript-eslint/consistent-type-imports
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
-  selector: 'app-mentor-login',
   standalone: true,
-  imports: [],
+  selector: 'app-mentor-login',
   templateUrl: './mentor-login.component.html',
-  styleUrl: './mentor-login.component.scss'
+  styleUrls: ['./mentor-login.component.scss']
 })
-export class MentorLoginComponent {
+export class MentorLoginComponent implements OnInit {
+  // eslint-disable-next-line @typescript-eslint/prefer-readonly
+  constructor (private route: ActivatedRoute) {}
 
+  ngOnInit (): void {
+    this.route.queryParams.subscribe((params) => {
+      const code = params['code']
+      if (code !== null && code !== undefined) {
+        console.log('GitHub code recibido:', code)
+      }
+    })
+  }
+
+  loginWithGitHub (): void {
+    const clientId = 'Ov23liatIZRCf3flyPtL'
+    const redirectUri = 'http://localhost:4200/ita-challenge/mentor'
+
+    const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=read:user user:email`
+
+    window.location.href = githubAuthUrl
+  }
 }
