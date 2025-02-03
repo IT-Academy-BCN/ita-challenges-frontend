3418197be9b374dfd3d9196deac3c97ddaa5b1c3
Ivan <ielizalde.dev@gmail.com>
2025-02-03 12:56:49 +0100

    manage the backend github login response

--- src/app/modules/mentor/mentor-login/mentor-login.component.ts
+++ src/app/modules/mentor/mentor-login/mentor-login.component.ts
@@ -3,7 +3,12 @@
 import { HttpClient } from '@angular/common/http'
 import { Component, OnInit } from '@angular/core'
 import { ActivatedRoute, Router } from '@angular/router'
-import { environment } from 'src/environments/environment.prod'
+import { environment } from 'src/environments/environment'
+
+interface GitHubAuthResponse {
+  isValid: boolean
+  username: string
+}
 
 @Component({
   standalone: true,
@@ -12,7 +17,11 @@ import { environment } from 'src/environments/environment.prod'
   styleUrls: ['./mentor-login.component.scss']
 })
 export class MentorLoginComponent implements OnInit {
-  constructor (private route: ActivatedRoute, private http: HttpClient, private router: Router) {}
+  constructor (
+    private route: ActivatedRoute,
+    private http: HttpClient,
+    private router: Router
+  ) {}
 
   ngOnInit (): void {
     this.route.queryParams.subscribe((params) => {
@@ -20,27 +29,44 @@ export class MentorLoginComponent implements OnInit {
       if (code !== null && code !== undefined) {
         console.log('GitHub code recibido:', code)
 
-        const url = environment.BACKEND_ITA_CHALLENGE_BASE_URL + environment.BACKEND_GITHUB_VALIDATE_ENDPOINT
+        const url =
+          environment.BACKEND_ITA_CHALLENGE_BASE_URL +
+          environment.BACKEND_GITHUB_VALIDATE_ENDPOINT
 
-        this.http.post(url, { code })
-          .subscribe({
-            next: (response: any) => {
-              console.log('GitHub backend response:', response)
+        this.http.post<GitHubAuthResponse>(url, { code }).subscribe({
+          next: (response) => {
+            console.log('GitHub backend response:', response)
 
-              if (response.status === 'success') {
-                void this.router.navigate(['/ita-challenge/challenges'])
-              } else {
-                console.error('no eres mentor, acceso denegado')
-              }
+            if (response.isValid) {
+              alert(`✅ Bienvenido, ${response.username}! Redirigiendo...`)
+              localStorage.setItem('username', response.username)
+              void this.router.navigate(['/ita-challenge/challenges'])
+            } else {
+              alert('❌ No eres mentor, acceso denegado.')
             }
-          })
+          },
+          error: (err) => {
+            if (err.status === 401) {
+              console.error(
+                '🚫 Error 401: No autorizado. El usuario no es mentor o el token es inválido.'
+              )
+            } else if (err.status === 500) {
+              console.error('💥 Error 500: Error interno en el servidor.')
+            } else {
+              console.error(
+                '❌ Error desconocido en la petición al backend:',
+                err
+              )
+            }
+          }
+        })
       }
     })
   }
 
   loginWithGitHub (): void {
-    const clientId = 'Ov23liatIZRCf3flyPtL'
-    const redirectUri = 'http://localhost:4200/ita-challenge/mentor'
+    const clientId = environment.GITHUB_CLIENT_ID
+    const redirectUri = environment.GITHUB_REDIRECT_URI
 
     const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=read:user user:email`
 
