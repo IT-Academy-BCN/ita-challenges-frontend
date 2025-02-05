5d7481150372fe393b0c27238958068f82da7a0e
deuvede24 <cdaniela.vd@gmail.com>
2025-02-05 14:23:59 +0100

    chore: remove unused Cypress test file for standalone component"

--- src/app/modules/challenge/components/challenge-form/challenge-form.component.cy.ts
+++
@@ -1,7 +0,0 @@
-/* import { ChallengeFormComponent } from './challenge-form.component'
-
-describe('ChallengeFormComponent', () => {
-  it('should mount', () => {
-    cy.mount(ChallengeFormComponent)
-  })
-}) */
