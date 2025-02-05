eeb960eb2baf0a4958c1a14c59486bd79704ca3c
deuvede24 <cdaniela.vd@gmail.com>
2025-02-05 11:53:38 +0100

    refactor: update onSubmit method with comments and remove unused code Comments to the `onSubmit` method to document future changes (mentor-specific path and guards). Removed commented-out code for better readability.

--- src/app/modules/challenge/components/challenge-form/challenge-form.component.ts
+++ src/app/modules/challenge/components/challenge-form/challenge-form.component.ts
@@ -1,49 +1,3 @@
-/* import { Component, inject } from '@angular/core'
-import { Router } from '@angular/router'
-import { ChallengeService } from 'src/app/services/challenge.service'
-import { type CreateChallenge } from '../../../../models/create-challenge.interface'
-
-@Component({
-  standalone: true,
-  selector: 'app-challenge-form',
-  templateUrl: './challenge-form.component.html',
-  styleUrls: ['./challenge-form.component.scss']
-})
-export class ChallengeFormComponent {
-  challenge: CreateChallenge = {
-    challengeTitle: '',
-    description: '',
-    level: 'EASY',
-    language: 'Java' as 'Java' | 'PHP' | 'Python' | 'JavaScript',
-    solution: ''
-  }
-
-  private readonly challengeService = inject(ChallengeService)
-  private readonly router = inject(Router)
-
-  private isFormValid (): boolean {
-    return (
-      this.challenge.challengeTitle.trim() !== '' &&
-        this.challenge.description.trim() !== '' &&
-        ['Java', 'PHP', 'Python', 'JavaScript'].includes(this.challenge.language) &&
-        this.challenge.solution.trim() !== ''
-    )
-  }
-
-  onSubmit (): void {
-    this.challengeService.createChallenge(this.challenge).subscribe({
-      next: (response) => {
-        console.log('Reto creado:', response)
-        // Redirigir al listado o mostrar mensaje de éxito
-        void this.router.navigate(['/ita-challenge/challenges'])
-      },
-      error: (err) => {
-        console.error('Error al crear el reto:', err)
-      }
-    })
-  }
-} */
-
 import { Component, inject } from '@angular/core'
 import { Router } from '@angular/router'
 import { ChallengeService } from 'src/app/services/challenge.service'
@@ -87,6 +41,11 @@ export class ChallengeFormComponent {
     this.challengeService.createChallenge(this.challenge).subscribe({
       next: (response) => {
         console.log('Reto creado:', response)
+        // Temporary: Redirects to /challenges for now.
+        // Future:
+        // - Only mentors will access this feature.
+        // - Redirect to mentor dashboard after creation.
+        // - Add a guard to restrict access to mentors.
         void this.router.navigate(['/ita-challenge/challenges'])
       },
       error: (err) => {
