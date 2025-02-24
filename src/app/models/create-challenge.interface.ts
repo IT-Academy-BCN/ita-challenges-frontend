c27fc03b7b10e2d005baa4f3ff3f5c2c437628d7
deuvede24 <cdaniela.vd@gmail.com>
2025-02-24 12:04:50 +0100

    feat: update CreateChallenge interface to support dynamic language input

--- src/app/models/create-challenge.interface.ts
+++ src/app/models/create-challenge.interface.ts
@@ -1,7 +1,14 @@
-export interface CreateChallenge {
+/* export interface CreateChallenge {
   challengeTitle: string
   description: string
   level: 'EASY' | 'MEDIUM' | 'HARD'
   language: 'Java' | 'PHP' | 'Python' | 'Javascript' | 'Typescript' | 'SQL'
+  solution: string
+} */
+export interface CreateChallenge {
+  challengeTitle: string
+  description: string
+  level: 'EASY' | 'MEDIUM' | 'HARD'
+  language: string
   solution: string
 }
