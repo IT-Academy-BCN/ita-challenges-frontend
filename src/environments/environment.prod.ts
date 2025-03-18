8f94fc6615a09df0101845efa3a9447bc769a461
Miriam Martin Casas <miriam.martin.casas@gmail.com>
2025-03-18 14:11:10 +0100

    changes environment settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -18,7 +18,7 @@ export const environment = {
   BACKEND_SSO_PATCH_USER: '/users',
   BACKEND_SSO_REGISTER_URL: '/auth/register',
   BACKEND_SSO_VALIDATE_TOKEN_URL: '/tokens/validate',
-  BACKEND_GITHUB_VALIDATE_ENDPOINT: '/auth/github/validate',
+  BACKEND_GITHUB_VALIDATE_ENDPOINT: '/auth/github/authenticate',
   ADMIN_USER: '../assets/dummy/admin-user.json',
   USER_SOLUTION: '../assets/dummy/user-solution.json',
   ITINERARY_ID: 'clpb8t1cc000008k0cg1icvl4',
