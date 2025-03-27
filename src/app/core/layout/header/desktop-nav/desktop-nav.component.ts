10f68b0a57f485ec34a7101ff56eca279c4c3ad4
GuillemBo <guillemb4b@gmail.com>
2025-03-27 14:07:14 +0100

    dropdown menu

--- src/app/core/layout/header/desktop-nav/desktop-nav.component.ts
+++ src/app/core/layout/header/desktop-nav/desktop-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core'
+import { Component, HostListener, Inject, OnInit } from '@angular/core'
 import { NavService } from 'src/app/services/nav.service'
 
 @Component({
@@ -6,11 +6,17 @@ import { NavService } from 'src/app/services/nav.service'
   templateUrl: './desktop-nav.component.html',
   styleUrl: './desktop-nav.component.scss'
 })
-export class DesktopNavComponent {
+export class DesktopNavComponent implements OnInit{
   isLoggedIn = false
+  dropdownOpen: boolean = false
+  user: string = ''
 
   constructor (@Inject(NavService) public navService: NavService) {
     this.isLoggedIn = !(localStorage.getItem('authToken') == null)
+  }
+
+  ngOnInit(): void {
+    this.getUserFromLocalStorage()
   }
 
   changeLanguage (event: Event): void {
@@ -19,7 +25,30 @@ export class DesktopNavComponent {
     this.navService.changeLanguage(language)
   }
 
+  toggleDropdown(): void {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
+    @HostListener('document:click', ['$event'])
+    onClickOutside(event: MouseEvent) {
+      const dropdown = document.querySelector('.dropdown');
+      const userButton = document.querySelector('.user');
+  
+      if (
+        !dropdown?.contains(event.target as Node) &&
+        !userButton?.contains(event.target as Node)
+      ) {
+        this.dropdownOpen = false;
+      }
+    }
+
   onLoginSuccess (isLogged: boolean): void {
     this.isLoggedIn = isLogged
+  }
+
+  getUserFromLocalStorage (): void {
+    const username = localStorage.getItem('username');
+    console.log('Username from localStorage:', username);
+    this.user = username || '';
   }
 }
