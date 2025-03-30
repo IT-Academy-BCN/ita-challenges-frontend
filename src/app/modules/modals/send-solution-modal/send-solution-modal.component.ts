eb8c1b55b2edcf58f9c92c84c0f2f19999fdcadd
Alvaro <alvaro.ledesmav@gmail.com>
2025-03-30 16:19:09 +0200

    feat(send-solution-modal): recieve info to make a PUT request for submitting challenge solution.

--- src/app/modules/modals/send-solution-modal/send-solution-modal.component.ts
+++ src/app/modules/modals/send-solution-modal/send-solution-modal.component.ts
@@ -1,6 +1,8 @@
 import { Component, inject, Input } from '@angular/core'
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap'
 import { SolutionService } from 'src/app/services/solution.service'
+import { ChallengeService } from '../../../services/challenge.service';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-send-solution-modal',
@@ -11,14 +13,52 @@ import { SolutionService } from 'src/app/services/solution.service'
 export class SendSolutionModalComponent {
   private readonly modalService = inject(NgbModal)
   private readonly solutionService = inject(SolutionService)
+  private readonly challengeService = inject(ChallengeService);
 
   @Input() idChallenge!: string
+  @Input() userId!: string;
+  languageId: string = ''; 
+  solutionText: string = '';
 
-  public acceptSolution (): void {
-    this.solutionService.updateSolutionSentState(true)
-    this.solutionService.sendSolutionText(true)
-    this.solutionService.activeIdSubject.next(2)
-    this.closeModal()
+  ngOnInit(): void {
+    this.getLanguageId(); 
+    this.getSolutionText(); 
+  }
+
+  private getLanguageId(): void {
+    this.challengeService.getChallengeById(this.idChallenge).subscribe({
+      next: (challenge) => {
+        this.languageId = challenge.languages[0].id_language;
+      },
+      error: (error) => {
+        console.error("Error al obtener Language ID:", error);
+      }
+    });
+  }
+
+  private getSolutionText(): void {
+    this.solutionText = localStorage.getItem('editorContent') || '';
+  }
+
+  public acceptSolution(): void {
+
+    this.solutionService.submitSolution(
+      this.idChallenge,
+      this.languageId,
+      this.solutionText,
+      this.userId,
+      "ENDED"
+    ).subscribe({
+      next: (response) => {
+        this.solutionService.updateSolutionSentState(true);
+        this.solutionService.sendSolutionText(true);
+        this.solutionService.activeIdSubject.next(2);
+        this.closeModal();
+      },
+      error: (error) => {
+        console.error('Error al enviar la solución:', error);
+      }
+    });
   }
 
   public closeModal (): void {
