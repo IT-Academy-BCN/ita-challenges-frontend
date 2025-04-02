445e4b9889a19e2c74596eb2a04f96df465c6557
GuillemBo <guillemb4b@gmail.com>
2025-04-02 13:08:59 +0200

    merge

--- src/app/core/layout/header/desktop-nav/desktop-nav.component.ts
+++ src/app/core/layout/header/desktop-nav/desktop-nav.component.ts
@@ -1,55 +1,42 @@
-<<<<<<< HEAD
-import { Component, HostListener, Inject, OnInit } from '@angular/core'
-=======
-import { Component, Inject, OnDestroy, OnInit } from '@angular/core'
+import { Component, HostListener, Inject, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
->>>>>>> develop
-import { AuthService } from 'src/app/services/auth.service'
-import { NavService } from 'src/app/services/nav.service'
+import { AuthService } from 'src/app/services/auth.service';
+import { NavService } from 'src/app/services/nav.service';
 
 @Component({
   selector: 'app-desktop-nav',
   templateUrl: './desktop-nav.component.html',
   styleUrl: './desktop-nav.component.scss'
 })
-<<<<<<< HEAD
-export class DesktopNavComponent implements OnInit{
-  isLoggedIn = false
-  dropdownOpen: boolean = false
-  user: string = ''
 
-  constructor (@Inject(NavService) public navService: NavService,
-              @Inject(AuthService) private _authService: AuthService) {
-    this.isLoggedIn = !(localStorage.getItem('authToken') == null)
-  }
-=======
 export class DesktopNavComponent implements OnInit, OnDestroy{
-  isLoggedIn = false
+  isLoggedIn = false;
+  dropdownOpen: boolean = false;
+  user: string = '';
   private authSubscription!: Subscription;
 
-  constructor (
-    @Inject(NavService) public navService: NavService,
-    @Inject(AuthService) private _authService: AuthService) {}
 
-    ngOnInit(): void {
-      this.authSubscription = this._authService.isLoggedIn$.subscribe(isLoggedIn => {
-        this.isLoggedIn = isLoggedIn;
-      });
-    }
+  constructor(
+    @Inject(NavService) public navService: NavService,
+    @Inject(AuthService) private _authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.authSubscription = this._authService.isLoggedIn$.subscribe(isLoggedIn => {
+      this.isLoggedIn = isLoggedIn;
+    });
+
+    this._authService.updateUserRoleFromToken();
+    this._authService.getUsername().subscribe((username) => {
+      this.user = username;
+    });
+  }
 
     ngOnDestroy(): void {
       if (this.authSubscription) {
         this.authSubscription.unsubscribe();
       }
     }
->>>>>>> develop
-
-  ngOnInit(): void {
-    this._authService.updateUserRoleFromToken();
-    this._authService.getUsername().subscribe((username) => {
-      this.user = username;
-    });
-  }
 
   changeLanguage (event: Event): void {
     const selectElement = event.target as HTMLSelectElement
