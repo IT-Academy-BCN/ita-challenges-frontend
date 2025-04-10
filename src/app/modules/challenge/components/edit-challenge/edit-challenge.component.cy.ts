2e2bd709d7ea78a6c00ad652ad6c328efd227fad
Marta Altimir <martaag18@gmail.com>
2025-04-10 11:31:07 +0200

    feat: add edit challenge route

---
+++ src/app/modules/challenge/components/edit-challenge/edit-challenge.component.cy.ts
@@ -0,0 +1,7 @@
+import { EditChallengeComponent } from './edit-challenge.component'
+
+describe('EditChallengeComponent', () => {
+  it('should mount', () => {
+    cy.mount(EditChallengeComponent)
+  })
+})
---
+++ src/app/modules/challenge/components/edit-challenge/edit-challenge.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-challenge',
+  templateUrl: './edit-challenge.component.html',
+  styleUrl: './edit-challenge.component.css'
+})
+export class EditChallengeComponent {
+
+}
