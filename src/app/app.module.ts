3e1777c19bed62904a3f1f1e3f76dedbfbeb6f12
Miranda <mirandacallejonh@gmail.com>
2025-04-23 16:00:03 +0200

    Remove commented code from navigation components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,19 +31,21 @@ export function HttpLoaderFactory (http: HttpClient): any {
   return new TranslateHttpLoader(http)
 }
 
-import { MentorLoginComponent } from './modules/mentor/mentor-login/mentor-login.component';
-
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   bootstrap: [AppComponent],
   imports: [BrowserModule,
+<<<<<<< HEAD
 <<<<<<< HEAD
     MentorLoginComponent,
 =======
     MentorLoginComponent,  // Add this line
 >>>>>>> b044dc05 (feat(auth): centralizar componente de login en app.component)
+=======
+    MentorLoginComponent,
+>>>>>>> 289ee697 (Remove commented code from navigation components)
     AppRoutingModule,
     BrowserAnimationsModule,
     CoreModule,
