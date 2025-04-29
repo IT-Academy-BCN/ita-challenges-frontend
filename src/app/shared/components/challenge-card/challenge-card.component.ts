00cc8b58d203c0a3901ee47b86ec6cd0fa87a3c2
Marta Altimir <martaag18@gmail.com>
2025-04-29 12:41:48 +0200

    refactor(favorites): simplify toggleFavorite logic using ternary operator

--- src/app/shared/components/challenge-card/challenge-card.component.ts
+++ src/app/shared/components/challenge-card/challenge-card.component.ts
@@ -50,11 +50,6 @@ export class ChallengeCardComponent implements OnInit {
   toggleFavorite (event: MouseEvent): void {
     event.stopPropagation()
     this.isFavorite = !this.isFavorite
-
-    if (this.isFavorite) {
-      this.favorites_count++
-    } else {
-      this.favorites_count = Math.max(0, this.favorites_count - 1)
-    }
+    this.favorites_count = this.isFavorite ? this.favorites_count + 1 : Math.max(0, this.favorites_count - 1)
   }
 }
