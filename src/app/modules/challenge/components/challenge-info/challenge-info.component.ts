bd86349c67f0e770ca0da9d80727cf2d86dca0c9
Miranda <mirandacallejonh@gmail.com>
2025-04-30 11:40:16 +0200

    NOSONAR coment updated

--- src/app/modules/challenge/components/challenge-info/challenge-info.component.ts
+++ src/app/modules/challenge/components/challenge-info/challenge-info.component.ts
@@ -82,10 +82,11 @@ implements OnInit {
 
   @Input() set description(value: string) {
     if (value) {
-      // NOSONAR: Using bypassSecurityTrustHtml is safe here because:
-      // 1. Content comes from a trusted source
-      // 2. Rich HTML is required for challenge description formatting
-      // 3. DomSanitizer is Angular's built-in security mechanism
+      // NOSONAR typescript:S6268
+      // Make sure disabling Angular built-in sanitization is safe here because:
+      // 1. Content comes from a trusted backend API with proper validation
+      // 2. Rich HTML is required for challenge description formatting (markdown/code blocks)
+      // 3. DomSanitizer is Angular's built-in security mechanism for HTML content
       this.safeDescription = this.sanitizer.bypassSecurityTrustHtml(value)
     }
   }
