fbbef65c340d4a503bc34e464d581d0f336bc8f6
bertapasamontes <bertapasamontes@gmail.com>
2025-05-08 14:02:50 +0200

    Remove pagination component files and update shared-component.module accordingly

--- src/app/shared/components/shared-components.module.ts
+++ src/app/shared/components/shared-components.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core'
 import { RouterModule } from '@angular/router'
 import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap'
 import { CommonModule } from '@angular/common'
-import { PaginationComponent } from './pagination/pagination.component'
 import { ChallengeCardComponent } from './challenge-card/challenge-card.component'
 import { ResourceCardComponent } from './resource-card/resource-card.component'
 import { SolutionComponent } from './solution/solution.component'
@@ -15,7 +14,6 @@ import { EditorModule } from '@tinymce/tinymce-angular'
 
 @NgModule({
   declarations: [
-    PaginationComponent,
     ChallengeCardComponent,
     ResourceCardComponent,
     SolutionComponent,
@@ -32,7 +30,6 @@ import { EditorModule } from '@tinymce/tinymce-angular'
     EditorModule
   ],
   exports: [
-    PaginationComponent,
     ChallengeCardComponent,
     ResourceCardComponent,
     SolutionComponent,
