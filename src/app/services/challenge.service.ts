1967dc4eed973ffe1f7b64f43f95ac83a4c941f6
Marta Altimir <martaag18@gmail.com>
2025-05-09 16:49:58 +0200

    refactor: use environment constant for challenge base path

--- src/app/services/challenge.service.ts
+++ src/app/services/challenge.service.ts
@@ -127,7 +127,7 @@ export class ChallengeService {
       'Content-Type': 'application/json',
       ...this.authService.getAuthHeaders()
     };
-    const url = `${environment.BACKEND_ITA_CHALLENGE_BASE_URL}/challenge/challenges/${challengeId}/favorites`;
+    const url = `${environment.BACKEND_ITA_CHALLENGE_BASE_URL}${environment.BACKEND_ALL_CHALLENGES_URL}/${challengeId}/favorites`;
     return this.http.delete<FavoriteResponse>(url, { headers }).pipe(
       map(response => {
         return response;
