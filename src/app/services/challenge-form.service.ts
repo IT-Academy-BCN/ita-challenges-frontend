8384ca91ff7f1b32555ea5b45bc8bd9c477bc7cb
Alberto Basanta <dasantades@gmail.com>
2025-06-04 22:53:06 +0200

    refactor(tags-service): remove outdated function and keep getTagsByLanguage

--- src/app/services/challenge-form.service.ts
+++ src/app/services/challenge-form.service.ts
@@ -20,14 +20,6 @@ export class ChallengeFormService {
     return this.http.get<{ results: Language[] }>(url, { headers })
   }
 
-  getTags (): Observable<TagResponse> {
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/json'
-    })
-    const url = `${environment.BACKEND_ITA_CHALLENGE_BASE_URL}${environment.BACKEND_ITA_CHALLENGE_TAGS}`
-    return this.http.get<TagResponse>(url, { headers })
-  }
-
   getTagsByLanguage (languageId: string): Observable<TagResponse> {
     const headers = new HttpHeaders({
       'Content-Type': 'application/json'
