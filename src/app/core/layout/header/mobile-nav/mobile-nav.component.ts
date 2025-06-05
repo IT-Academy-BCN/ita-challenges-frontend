180294c0921db5e0edee5fe9461fa3c71ffa06e2
bertapasamontes <bertapasamontes@gmail.com>
2025-06-05 13:48:24 +0200

    refactor(mobile html): updated html to be as like as desktop, refactor(mobile ts): updated toggle to work. rebase went wrong and deleted important code

--- src/app/core/layout/header/mobile-nav/mobile-nav.component.ts
+++ src/app/core/layout/header/mobile-nav/mobile-nav.component.ts
@@ -27,14 +27,20 @@ export class MobileNavComponent implements OnInit, OnDestroy{
     });
 
     this._authService.updateUserRoleAndUserNameFromToken();
+    this.loadUserBasicDataMobile()
+    this._authService.checkAndHandleExpiredToken()
+  }
+  loadUserBasicDataMobile():void{
+    this._authService.updateUserRoleAndUserNameFromToken();
     this._authService.getUsername().subscribe((username) => {
       this.user = username;
     });
-
+    this._authService.getUserRole().subscribe((userRole) => {
+      this.currentRole = userRole
+    }) 
     this._authService.getUserPhoto().subscribe((userPhoto) => {
       this.userPhoto = userPhoto
     })
-    this._authService.checkAndHandleExpiredToken()
   }
 
     ngOnDestroy(): void {
