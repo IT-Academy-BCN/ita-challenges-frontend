bd3cec8e7db8cb9937b0e12e55eb5de936c9be72
Victor Redondo <vrviktor@gmail.com>
2025-06-06 01:27:25 +0200

    chore: reformat TranslateModule.forRoot configuration for readability

--- src/app/shared/components/challenge-card/challenge-card.component.spec.ts
+++ src/app/shared/components/challenge-card/challenge-card.component.spec.ts
@@ -50,8 +50,7 @@ describe('ChallengeCardComponent', () => {
         RouterTestingModule,
         HttpClientTestingModule,
         TranslateModule.forRoot({
-          loader: { provide: TranslateLoader, useFactory: HttpLoaderFactory, deps: [HttpClient],
-          },
+          loader: { provide: TranslateLoader, useFactory: HttpLoaderFactory, deps: [HttpClient] }
         }),
       ],
       providers: [
