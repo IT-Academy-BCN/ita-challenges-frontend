eb3d1c6541ec37f50f7c5a53af7faaa792ff76b8
Victor Redondo <vrviktor@gmail.com>
2025-06-16 18:47:16 +0200

    chore(starter.service.spec): sync test file with develop version

--- src/app/services/starter.service.spec.ts
+++ src/app/services/starter.service.spec.ts
@@ -92,9 +92,7 @@ describe('StarterService', () => {
   it('Should stream all challenges', (done) => {
     const mockResponse: Record<string, unknown> = { challenge: 'challenge' }
     service.getAllChallenges().subscribe()
-    const req = httpClientMock.expectOne(
-      `${environment.BACKEND_ITA_CHALLENGE_BASE_URL}${environment.BACKEND_ALL_CHALLENGES_URL}`
-    );
+    const req = httpClientMock.expectOne(`${environment.BACKEND_ITA_CHALLENGE_BASE_URL}${environment.BACKEND_ALL_CHALLENGES_URL}`)
     expect(req.request.method).toEqual('GET')
     req.flush(mockResponse)
     done()
