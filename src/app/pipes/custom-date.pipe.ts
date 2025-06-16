7f7a01d48297e5e500151345755a824f876f2591
Victor Redondo <vrviktor@gmail.com>
2025-06-16 13:48:14 +0200

    chore(conflict): adapt customDate pipe to use current language and update test accordingly

--- src/app/pipes/custom-date.pipe.ts
+++ src/app/pipes/custom-date.pipe.ts
@@ -1,16 +1,27 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core'
+import { TranslateService } from '@ngx-translate/core'
 
 @Pipe({
   name: "customDate",
   standalone: true,
 })
 export class CustomDatePipe implements PipeTransform {
-  private months = ["Gen", "Feb", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Oct", "Nov", "Des"]
+  private monthNames: Record<string, string[]> = {
+    es: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+    en: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    ca: ['Gen', 'Feb', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Oct', 'Nov', 'Des']
+  }
+
+  constructor(private translate: TranslateService) {}
+
   transform(value: Date | string): string {
     const date = new Date(value)
     const day = date.getDate()
-    const month = this.months[date.getMonth()]
+    const monthIndex = date.getMonth()
     const year = date.getFullYear()
-    return `${day} ${month} ${year}`
+    const lang = this.translate.currentLang || 'es'
+    const months = this.monthNames[lang] || this.monthNames['es']
+
+    return `${day} ${months[monthIndex]} ${year}`
   }
 }
--- src/app/shared/components/challenge-card/challenge-card.component.spec.ts
+++ src/app/shared/components/challenge-card/challenge-card.component.spec.ts
@@ -4,12 +4,12 @@ import { RouterTestingModule } from '@angular/router/testing'
 import { StarterService } from '../../../services/starter.service'
 import { provideHttpClient, withInterceptorsFromDi, HttpClient } from '@angular/common/http'
 import { provideHttpClientTesting, HttpClientTestingModule } from '@angular/common/http/testing'
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core'
+import { TranslateModule, TranslateLoader, TranslateService  } from '@ngx-translate/core'
 
 import { HttpLoaderFactory } from '../../../app.module' // Asegúrate de que la ruta es correcta
 import { LOCALE_ID, Pipe, type PipeTransform } from '@angular/core'
 import { By } from '@angular/platform-browser'
-import { formatDate } from '@angular/common'
+import { formatDate } from "@angular/common";
 import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap'
 import { AuthService } from 'src/app/services/auth.service'
 import { ChallengeService } from 'src/app/services/challenge.service'
@@ -28,6 +28,7 @@ describe('ChallengeCardComponent', () => {
   let fixture: ComponentFixture<ChallengeCardComponent>
   let mockChallengeService: jest.Mocked<ChallengeService>
   let mockAuthService: jest.Mocked<AuthService>
+  let datePipe: CustomDatePipe
 
   beforeEach(async () => {
     mockChallengeService = {
@@ -55,6 +56,7 @@ describe('ChallengeCardComponent', () => {
       ],
       providers: [
         StarterService,
+        CustomDatePipe,
         { provide: LOCALE_ID, useValue: 'ca' },
         { provide: ChallengeService, useValue: mockChallengeService },
         { provide: AuthService, useValue: mockAuthService }
@@ -63,6 +65,7 @@ describe('ChallengeCardComponent', () => {
 
     fixture = TestBed.createComponent(ChallengeCardComponent)
     component = fixture.componentInstance
+    datePipe = TestBed.inject(CustomDatePipe)
     fixture.detectChanges()
   })
 
