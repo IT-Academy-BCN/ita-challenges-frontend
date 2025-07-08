93ccf90c19c7f779b4c4ede29a3bbf6842b39c2c
oscarrep <oscar.bcn.95@gmail.com>
2025-07-08 19:32:10 +0200

    feat: Add registration error and success feedback

--- src/app/modules/modals/register-users-modal/register-users-modal.component.ts
+++ src/app/modules/modals/register-users-modal/register-users-modal.component.ts
@@ -11,8 +11,11 @@ export class RegisterUsersModalComponent {
   private readonly registerUsersService = inject(RegisterUsersService);
   username: string = '';
   usernames: string[] = [];
+  registrationError = false;
+  registrationSuccess = false;
+  pendingResponses = 0;
 
-  public closeModal (): void {
+  public closeModal(): void {
     this.modalService.dismissAll()
   }
 
@@ -33,18 +36,33 @@ export class RegisterUsersModalComponent {
     return this.usernames.some(u => this.normalizeUsername(u) === normalized);
   }
 
- confirmRegistration():void {
-  this.usernames.forEach(username =>{
-    this.registerUsersService.registerUserMock(username).subscribe({
-      next: (res) => {
-        console.log(`User ${username} registered successfully`, res);
-      },
-      error: (err) => {
-        console.error(`Error registering user ${username}`, err);
-      }
-    });
-  })
-  this.usernames = [];
-  this.closeModal();
- }
+  confirmRegistration(): void {
+    this.registrationError = false;
+    this.registrationSuccess = false;
+    this.pendingResponses = this.usernames.length;
+
+    this.usernames.forEach(username => {
+      this.registerUsersService.registerUserMock(username).subscribe({
+        next: (res) => {
+          console.log(`User ${username} registered successfully`, res);
+          this.checkIfRegistrationCompleted();
+        },
+        error: (err) => {
+          console.error(`Error registering user ${username}`, err);
+          this.registrationError = true;
+          this.checkIfRegistrationCompleted();
+        }
+      });
+    })
+  }
+
+  checkIfRegistrationCompleted(): void {
+    this.pendingResponses--;
+
+    if (this.pendingResponses === 0) {
+      if (!this.registrationError) {
+        this.registrationSuccess = true;
+        this.usernames = [];
+      }    }
+  }
 }
