2b1645e66074dd11a98e32eb5609f5796e55925f
oscarrep <oscar.bcn.95@gmail.com>
2025-07-09 11:36:10 +0200

    fix: fix version to 3.1.49 and correct backend URL in register user service

--- src/app/services/register-users.service.ts
+++ src/app/services/register-users.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { delay, Observable, of, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment'
 
 @Injectable({ providedIn: 'root' })
 export class RegisterUsersService {
   constructor(private readonly http: HttpClient) { }
 
   registerUser(username: string): Observable<any> {
-    return this.http.post('{BACKEND_ITA_CHALLENGE_BASE_URL}/{CREATE_USER}', { username });
+    return this.http.post(`${environment.BACKEND_ITA_CHALLENGE_BASE_URL}/${environment.CREATE_USER}`, { username });
   }
 
   registerUserMockSuccess(username: string): Observable<any> {
