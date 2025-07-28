aa7a7fc1ab509a3dd121197b604327cfe2961c36
oscarrep <oscar.bcn.95@gmail.com>
2025-07-28 18:39:41 +0200

    added back CREATE_USER

--- src/app/services/register-users.service.ts
+++ src/app/services/register-users.service.ts
@@ -8,7 +8,7 @@ export class RegisterUsersService {
   constructor(private readonly http: HttpClient) { }
 
   registerUser(username: string): Observable<any> {
-    return this.http.post(`${environment.BACKEND_ITA_CHALLENGE_BASE_URL}/admin/users/create`, { username });
+    return this.http.post(`${environment.BACKEND_ITA_CHALLENGE_BASE_URL}${environment.CREATE_USER}`, { username });
   }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -40,4 +40,5 @@ export const environment = {
   AUTH_BASIC_URL: 'http://dev.ita-challenges.eurecatacademy.org/ita-challenge/',
   AUTH_SWITCH_ROLE: '/auth/switch-role',
   GITHUB_PROFILE_URL: 'https://api.github.com/users/',
+  CREATE_USER: '/admin/users/create',
 }
