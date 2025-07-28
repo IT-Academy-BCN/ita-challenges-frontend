4a09e3ce06b852e410a4293f3b7ea84eeaadce64
oscarrep <oscar.bcn.95@gmail.com>
2025-07-28 14:27:39 +0200

    fix environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 import { version } from '../../package.json'
 
+const ADMIN_USER_CREATE_PATH = '/admin/users/create';
+
 export const environment = {
   production: true,
   appVersion: version,
@@ -40,7 +42,5 @@ export const environment = {
   AUTH_BASIC_URL: 'http://dev.ita-challenges.eurecatacademy.org/ita-challenge/',
   AUTH_SWITCH_ROLE: '/auth/switch-role',
   GITHUB_PROFILE_URL: 'https://api.github.com/users/',
-  ADMIN: {
-    CREATE_USER: '/admin/users/create',
-  },
+  CREATE_USER: ADMIN_USER_CREATE_PATH,
 }
