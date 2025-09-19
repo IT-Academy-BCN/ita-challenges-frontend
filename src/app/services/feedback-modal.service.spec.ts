b18af26e23fbdd2c2b8fb955619078c4961c10fe
Sofia Ayala <sofiarayala1995@gmail.com>
2025-09-19 16:24:59 +0200

    feat: added feedback-modal service and installed sweetalert2 library

---
+++ src/app/services/feedback-modal.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { FeedbackModalService } from './feedback-modal.service';
+
+describe('FeedbackModalService', () => {
+  let service: FeedbackModalService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(FeedbackModalService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
