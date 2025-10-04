cac9bb8e4c7daad27bc056543c839c035a340665
VictorGonPe <victor.gonzalez.perez.1982@gmail.com>
2025-10-04 20:49:30 +0200

    refactor(filter-models): clean FilterChallenge model and add optional tags

--- src/app/models/filter-challenge.model.ts
+++ src/app/models/filter-challenge.model.ts
@@ -1,11 +1,6 @@
 export class FilterChallenge {
-  languages: string[]
-  levels: string[]
-  progress: number[]
-
-  constructor (element: any) {
-    this.languages = element.id_language
-    this.levels = element.id_language
-    this.progress = element.id_language
-  }
+  languages: string[] = []
+  levels: string[] = []
+  progress: number[] = []
+  tags?: string[] = []
 }
