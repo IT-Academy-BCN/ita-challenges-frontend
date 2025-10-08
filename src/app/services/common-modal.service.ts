7a27f02e66f54662f3277822532bdb4d5cdc489f
Leoyalta <149539157+Leoyalta@users.noreply.github.com>
2025-10-08 12:23:58 +0200

    Update common-modal.service.ts

--- src/app/services/common-modal.service.ts
+++ src/app/services/common-modal.service.ts
@@ -7,26 +7,20 @@ import Swal, { SweetAlertOptions, SweetAlertResult } from "sweetalert2";
 export class CommonModalService {
   constructor() {}
 
-  loginRequestModal(): Promise<SweetAlertResult> {
+  loginRequestModal() {
     const options: SweetAlertOptions = {
       title: "Inicio de sesión requerido",
       text: "En caso de que no estés dado de alta, contacta con tu mentor",
       showCloseButton: true,
-      showConfirmButton: true,
-      showCancelButton: true,
-      confirmButtonText: "Ir a login",
-      cancelButtonText: "Cancelar",
+      showConfirmButton: false, 
+      showCancelButton: false,  
       allowOutsideClick: true,
       customClass: {
         popup: "custom-modal",
         title: "custom-title",
         htmlContainer: "custom-html-container",
-        confirmButton: "custom-confirm-button",
-        cancelButton: "custom-cancel-button",
       },
     };
-    return Swal.fire(options);
-  }
 
   loadingPostingChallengeModal(): Promise<SweetAlertResult> {
     const options: SweetAlertOptions = {
