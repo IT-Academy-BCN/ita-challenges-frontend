da452acb230e974a73addeff7eb692f2fada86a5
Ilmira Dozhdikova <dozhdikovailmira@gmail.com>
2025-11-24 09:47:11 +0100

    fix: sonar issues code duplications in solutionService.spec.ts
    # Conflicts:
    #	src/app/services/solution.service.spec.ts

--- src/app/services/solution.service.spec.ts
+++ src/app/services/solution.service.spec.ts
@@ -121,17 +121,14 @@ describe('SolutionService', () => {
     req.flush(mockUserSolution)
   })
 
-  it('should send the correct data in PUT request', () => {
-  const uuid_challenge = 'f6e0f877-9560-4e68-bab6-7dd5f16b46a5';
-  const uuid_language = '660e1b18-0c0a-4262-a28a-85de9df6ac5f';
-  const uuid_user = 'user123';
-  const solution_text = 'Mi solución de prueba';
-  const action = 'COMPLETED';
-
+  it('should send the correct data in PUT request', (done) => {
   const mockResponse = { success: true };
 
-  service.submitSolution(uuid_challenge, uuid_language, uuid_user, action, solution_text).subscribe(response => {
+  service.submitSolution(uuid_challenge, uuid_language, uuid_user, action, solution_text).subscribe({
+    next: (response) => {
     expect(response.success).toBe(true);
+    done();
+    }
   });
 
   const req = httpMock.expectOne(`${environment.BACKEND_ITA_CHALLENGE_BASE_URL}${environment.BACKEND_ITA_CHALLENGE_USER_SOLUTION}`);
@@ -156,10 +153,12 @@ describe('SolutionService', () => {
   
     const mockResponse = { success: true, message: 'Solution submitted successfully' };
   
-    service.submitSolution(uuid_challenge, uuid_language, uuid_user, action, solution_text).subscribe(response => {
+    service.submitSolution(uuid_challenge, uuid_language, uuid_user, action, solution_text).subscribe({
+      next: (response) => {
       expect(response.success).toBe(true);
       expect(response.message).toBe('Solution submitted successfully');
       done();
+      }
     });
   
     const req = httpMock.expectOne(`${environment.BACKEND_ITA_CHALLENGE_BASE_URL}${environment.BACKEND_ITA_CHALLENGE_USER_SOLUTION}`);
@@ -186,14 +185,14 @@ describe('SolutionService', () => {
 
   const mockError = { status: 500, statusText: 'Internal Server Error' };
 
-  service.submitSolution(uuid_challenge, uuid_language, uuid_user, solution_text, action).subscribe(
-    () => fail('Expected error, but got success response'),
-    (error) => {
+  service.submitSolution(uuid_challenge, uuid_language, uuid_user, action, solution_text).subscribe({
+   next: () => fail('Expected error, but got success response'),
+   error: (error) => {
       expect(error.status).toBe(500);
       expect(error.statusText).toBe('Internal Server Error');
       done();
     }
-  );
+  });
 
   const req = httpMock.expectOne(`${environment.BACKEND_ITA_CHALLENGE_BASE_URL}${environment.BACKEND_ITA_CHALLENGE_USER_SOLUTION}`);
   expect(req.request.method).toBe('PUT');
