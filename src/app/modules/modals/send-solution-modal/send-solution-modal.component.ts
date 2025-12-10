0603f10b70bd8a14852aa5eec160297dfccbc255
FlavioKde <flaviodavirro@gmail.com>
2025-12-10 13:37:20 +0100

    fix(modal): clean unused code in send-solution-modal.component

--- src/app/modules/modals/send-solution-modal/send-solution-modal.component.ts
+++ src/app/modules/modals/send-solution-modal/send-solution-modal.component.ts
@@ -2,11 +2,10 @@ import { Component, EventEmitter, inject, Input, Output } from '@angular/core'
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap'
 import { SolutionService } from 'src/app/services/solution.service'
 import { ChallengeService } from '../../../services/challenge.service';
-import { AuthService } from '../../../services/auth.service';
-import { SolutionStatus } from 'src/app/models/user-solution-status.enum';
 import { ChallengeTab } from 'src/app/shared/enums/challenge-tab.enum';
 import { SubmitSolutionResponse } from 'src/app/models/user-solution.interface';
 import { SolutionAction } from 'src/app/models/user-solution-action.enum'
+
 
 @Component({
   selector: 'app-send-solution-modal',
