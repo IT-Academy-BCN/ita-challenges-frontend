7b5487225e7dffffd8944f66467fac64af0c16ff
Arnau <arnauperezesteve@gmail.com>
2026-01-26 12:37:04 +0100

    fix(auth): store switched role token in sessionStorage fix(auth): attach Authorization header using sessionStorage token

--- src/app/interceptors/jwt-interceptor.ts
+++ src/app/interceptors/jwt-interceptor.ts
@@ -3,16 +3,14 @@ import { type HttpRequest, type HttpHandler, type HttpEvent, type HttpIntercepto
 import { type Observable } from 'rxjs'
 // import { TokenService } from '../services/token.service'
 import { environment } from 'src/environments/environment'
-import { CookieService } from 'ngx-cookie-service'
 
 @Injectable({
   providedIn: 'root'
 })
 export class JwtInterceptor implements HttpInterceptor {
-  private readonly cookieService = inject(CookieService)
 
   intercept (request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token: string = this.cookieService.get('authToken')
+    const token =sessionStorage.getItem('authToken') ?? ''
     const isApiUrl = request.url.startsWith(environment.BACKEND_ITA_CHALLENGE_BASE_URL)
     if (isApiUrl && token !== '') {
       // Agregar el token al encabezado Authorization
