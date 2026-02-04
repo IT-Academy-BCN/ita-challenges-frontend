c33698300a5bdddd57ca91bbedbcbc3a800fa28b
Giu Eminente <giulemi210@gmail.com>
2026-02-04 21:35:51 +0100

    language filter is now emitting correct output to bind it to wrapper

--- src/app/modules/starter/components/language-filter/language-filter.component.ts
+++ src/app/modules/starter/components/language-filter/language-filter.component.ts
@@ -1,32 +1,76 @@
-import { Component, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Language } from 'src/app/models/language.model';
-import { ChallengeService } from 'src/app/services/challenge.service';
+import { Component, EventEmitter, Output, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, FormControl } from "@angular/forms";
+import { Language } from "src/app/models/language.model";
+import { ChallengeService } from "src/app/services/challenge.service";
 
 @Component({
-  selector: 'app-language-filter',
-  templateUrl: './language-filter.component.html',
-  styleUrl: './language-filter.component.css'
+  selector: "app-language-filter",
+  templateUrl: "./language-filter.component.html",
+  styleUrl: "./language-filter.component.css",
 })
-
-
-export class LanguageFilterComponent {
-  languages: Language[] = [];
+export class LanguageFilterComponent implements OnInit {
+  
   mockLanguages: Language[] = [
-    { id_language: '1', language_name: 'JavaScript' },
-    { id_language: '2', language_name: 'Python' },
-    { id_language: '3', language_name: 'Java' },
-    { id_language: '4', language_name: 'PHP' }
+    { id_language: "1", language_name: "JavaScript" },
+    { id_language: "2", language_name: "Python" },
+    { id_language: "3", language_name: "Java" },
+    { id_language: "4", language_name: "PHP" },
   ];
+  
 
-  constructor(readonly challengeService: ChallengeService) {
-    this.challengeService.getAllLanguages().subscribe((res: any) => {
-      if (res.results !== undefined && res.status === 200) {
-        this.languages = res.results 
-      }  else {
-        this.languages = this.mockLanguages
-      }  
-      });
+  languages: Language[] = this.mockLanguages;
+  @Output() languageSelected = new EventEmitter<string[]>();
+  
+  languageForm: FormGroup;
+ 
+  constructor(
+    private readonly challengeService: ChallengeService,
+    private readonly fb: FormBuilder
+  ) {
+    this.languageForm = this.createFormFromLanguages(this.mockLanguages);
   }
 
+  ngOnInit(): void {
+    this.loadLanguages();
+  }
+
+  loadLanguages(): void {
+    this.challengeService.getAllLanguages().subscribe({
+      next: (res: any) => {
+        if (res.results !== null && res.results.length > 0) {
+          this.languages = res.results;
+          this.languageForm = this.createFormFromLanguages(res.results);
+        }
+      },
+      error: (err) => {
+        console.error('Error Fetching languages, using mocking data:', err);
+      }   
+    });
+  }
+
+  createFormFromLanguages(languages: Language[]): FormGroup {
+    const controls: { [key: string]: FormControl } = {};
+    
+    languages.forEach((language) => {
+      controls[language.language_name.toLowerCase()] = new FormControl(false);
+    });
+    
+    const form = this.fb.group(controls);
+    
+
+    form.valueChanges.subscribe(() => {
+
+      this.emitSelectedLanguages(form);
+    });
+    
+    return form;
+  }
+
+  emitSelectedLanguages(form: FormGroup): void {
+    const selectedLanguages = Object.keys(form.value)
+      .filter(key => form.value[key] === true);
+    
+    this.languageSelected.emit(selectedLanguages);
+    console.log('event emitted', selectedLanguages)
+  }
 }
