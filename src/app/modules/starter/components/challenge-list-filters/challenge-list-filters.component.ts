014190c13ed72e7182e2cb8f69c43722107b1566
Ot Roca <otrocadev@gmail.com>
2026-02-06 10:03:34 +0100

    refactor(starter): change challenge-list-filters component stylesheet format from CSS to SCSS to mantain consistency in the project

--- src/app/modules/starter/components/challenge-list-filters/challenge-list-filters.component.ts
+++ src/app/modules/starter/components/challenge-list-filters/challenge-list-filters.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-challenge-list-filters',
   templateUrl: './challenge-list-filters.component.html',
-  styleUrl: './challenge-list-filters.component.css'
+  styleUrl: './challenge-list-filters.component.scss'
 })
 export class ChallengeListFiltersComponent {
 
