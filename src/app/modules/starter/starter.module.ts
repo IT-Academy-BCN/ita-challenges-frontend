9960c6b867366f1ab96f6822e4cdb0c16f9ccf47
Giu Eminente <giulemi210@gmail.com>
2026-02-11 14:19:57 +0100

    rebased with new incoming changes

--- src/app/modules/starter/starter.module.ts
+++ src/app/modules/starter/starter.module.ts
@@ -19,7 +19,7 @@ import { ChallengeListFiltersComponent } from './components/challenge-list-filte
   declarations: [
     StarterComponent,
     StarterFiltersComponent,
-    LanguageFilterComponent
+    LanguageFilterComponent,
     ChallengeListFiltersComponent
   ],
   imports: [
