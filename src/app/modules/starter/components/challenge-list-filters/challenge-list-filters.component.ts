c93a9efa8cca26034cc6b896437221e37253ec49
vaniaferreresteban <vaniaferreresteban@gmail.com>
2026-02-16 12:22:59 +0100

    fix: resolve conflic issues

--- src/app/modules/starter/components/challenge-list-filters/challenge-list-filters.component.ts
+++ src/app/modules/starter/components/challenge-list-filters/challenge-list-filters.component.ts
@@ -1,16 +1,24 @@
-import { Component, EventEmitter, Output } from '@angular/core'
+import { Component, EventEmitter, Input, Output } from '@angular/core'
+import { type FilterChallenge } from 'src/app/models/filter-challenge.model'
+
+type ModalFilters = Pick<FilterChallenge, 'levels' | 'tags' | 'progress'>
 
 @Component({
   selector: 'app-challenge-list-filters',
   templateUrl: './challenge-list-filters.component.html',
-  styleUrl: './challenge-list-filters.component.scss'
+  styleUrls: ['./challenge-list-filters.component.scss']
 })
 export class ChallengeListFiltersComponent {
+  @Input() initialFilters: FilterChallenge = { languages: [], levels: [], progress: [], tags: [] }
+  @Output() filtersApplied = new EventEmitter<ModalFilters>()
+  @Output() sortSelected = new EventEmitter<string>()
+  @Output() orderSelected = new EventEmitter<boolean>()
   sortBy: string = 'popularity'
   isAscending: boolean = false
 
-  @Output() sortSelected = new EventEmitter<string>()
-  @Output() orderSelected = new EventEmitter<boolean>()
+  protected onModalFiltersApplied (filters: ModalFilters): void {
+    this.filtersApplied.emit(filters)
+  }
 
   changeSort (sort: string): void {
     this.sortBy = sort
