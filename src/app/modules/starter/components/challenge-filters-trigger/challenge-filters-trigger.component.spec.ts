467cd550c21af2b08b76926fd500c68d5420ebce
Arnau <arnauperezesteve@gmail.com>
2026-02-17 21:01:36 +0100

    test(filters): update modal config expectations after enabling Escape/backdrop close

--- src/app/modules/starter/components/challenge-filters-trigger/challenge-filters-trigger.component.spec.ts
+++ src/app/modules/starter/components/challenge-filters-trigger/challenge-filters-trigger.component.spec.ts
@@ -138,8 +138,8 @@ describe('ChallengeFiltersTriggerComponent', () => {
     expect(callArgs[1]).toEqual(
       jasmine.objectContaining({
         windowClass: 'challenge-filters-trigger-modal',
-        backdrop: 'static',
-        keyboard: false
+        backdrop: true,
+        keyboard: true
       })
     )
   })
