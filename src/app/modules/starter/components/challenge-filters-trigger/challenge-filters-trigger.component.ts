4a5f1e1482f4e000682b63fead58d83c4cf53e20
Arnau <arnauperezesteve@gmail.com>
2026-02-17 20:53:44 +0100

    feat(filters): enable Escape and backdrop close on ChallengeFiltersTrigger modal

--- src/app/modules/starter/components/challenge-filters-trigger/challenge-filters-trigger.component.ts
+++ src/app/modules/starter/components/challenge-filters-trigger/challenge-filters-trigger.component.ts
@@ -98,8 +98,8 @@ export class ChallengeFiltersTriggerComponent {
 
     this.modalService.open(this.modalTemplate, {
       windowClass: 'challenge-filters-trigger-modal',
-      backdrop: 'static',
-      keyboard: false
+      backdrop: true,
+      keyboard: true
     })
 
     setTimeout(() => {
