49510f1bce5b9522971b404c8d1968fcbb89a05c
Ot Roca <otrocadev@gmail.com>
2026-03-18 13:22:24 +0100

    refactor(starter): Replace provisional loading message with loading-card component

---
+++ src/app/shared/components/loading-card/loading-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-card',
+  templateUrl: './loading-card.component.html',
+  styleUrl: './loading-card.component.scss'
+})
+export class LoadingCardComponent {
+
+}
